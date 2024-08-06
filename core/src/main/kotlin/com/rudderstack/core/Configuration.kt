8fb38f9298fc1b6918619edf5a3f7325472abd6a
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-08-06 17:50:28 +0530

    refactor: move coroutine in configuration
    This is to facilitate the access of different coroutine configuration in different modules.

--- core/src/main/kotlin/com/rudderstack/core/Configuration.kt
+++ core/src/main/kotlin/com/rudderstack/core/Configuration.kt
@@ -2,10 +2,17 @@ package com.rudderstack.core
 
 import com.rudderstack.core.internals.logger.KotlinLogger
 import com.rudderstack.core.internals.logger.Logger
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 
 open class Configuration @JvmOverloads constructor(
     open val writeKey: String,
     open val dataPlaneUrl: String,
     open val logger: Logger = KotlinLogger(initialLogLevel = Logger.DEFAULT_LOG_LEVEL),
-    open var optOut: Boolean = false,
+    open var offline: Boolean? = false,
 )
+interface CoroutineConfiguration {
+    val analyticsScope: CoroutineScope
+    val analyticsDispatcher: CoroutineDispatcher
+    val networkDispatcher: CoroutineDispatcher
+}
