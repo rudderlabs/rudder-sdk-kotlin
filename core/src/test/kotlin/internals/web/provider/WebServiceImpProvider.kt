662cf53ead5b5aa2c37e23a924cc07185131caf9
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-08-12 21:28:24 +0530

    test: improve and refactor web module test cases

--- core/src/test/kotlin/internals/web/provider/WebServiceImpProvider.kt
+++ core/src/test/kotlin/internals/web/provider/WebServiceImpProvider.kt
@@ -1,20 +1,45 @@
 package internals.web.provider
 
+import com.rudderstack.core.Constants.DEFAULT_GZIP_STATUS
 import com.rudderstack.core.internals.web.HttpURLConnectionFactory
 import com.rudderstack.core.internals.web.WebServiceImpl
 import java.net.HttpURLConnection
 
-fun provideWebServiceImpl(
-    baseUrl: String = "https://api.example.com",
-    authHeaderString: String = "auth-token",
-    isGZIPEnabled: Boolean = false,
-    anonymousIdHeaderString: String = "anonymousId",
+fun provideWebServiceImplForGetRequest(
     connectionFactory: HttpURLConnectionFactory,
-) = WebServiceImpl(
+    baseUrl: String = "https://api.example.com",
+    endPoint: String = "/test",
+    authHeaderString: String = "auth-header",
+    query: Map<String, String> = mapOf(
+        "p" to "android",
+        "v" to "2.0",
+        "bv" to "34",
+    ),
+    customHeaders: Map<String, String> = emptyMap(),
+) = WebServiceImpl.getRequest(
     baseUrl = baseUrl,
+    endPoint = endPoint,
+    authHeaderString = authHeaderString,
+    query = query,
+    customHeaders = customHeaders,
+    connectionFactory = connectionFactory,
+)
+
+fun provideWebServiceImplForPostRequest(
+    connectionFactory: HttpURLConnectionFactory,
+    baseUrl: String = "https://api.example.com",
+    endPoint: String = "/test",
+    authHeaderString: String = "auth-header",
+    isGZIPEnabled: Boolean = DEFAULT_GZIP_STATUS,
+    anonymousIdHeaderString: String = "anonymous-id",
+    customHeaders: Map<String, String> = emptyMap(),
+) = WebServiceImpl.postRequest(
+    baseUrl = baseUrl,
+    endPoint = endPoint,
     authHeaderString = authHeaderString,
     isGZIPEnabled = isGZIPEnabled,
     anonymousIdHeaderString = anonymousIdHeaderString,
+    customHeaders = customHeaders,
     connectionFactory = connectionFactory,
 )
 
