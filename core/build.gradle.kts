bd513419c2f461597e648869d88c1ba80b531f43
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-08-26 20:33:29 +0530

    build(core): add coroutines test library

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -27,4 +27,5 @@ dependencies {
     testImplementation(libs.mockito.kotlin)
     testImplementation(libs.mockk)
     testImplementation(libs.json.assert)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
