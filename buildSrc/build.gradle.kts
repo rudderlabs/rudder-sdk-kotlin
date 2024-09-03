8e6b5ce69d7cedc7f16723044c13b41f0f2c8397
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-09-03 17:14:57 +0530

    chore: fix the buildSrc/RudderStackBuildConfig import
    This involves creating the `src` folder and also updating few fields.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,6 @@ repositories {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
--- buildSrc/src/main/kotlin/RudderStackBuildConfig.kt
+++ buildSrc/src/main/kotlin/RudderStackBuildConfig.kt
@@ -1,27 +1,33 @@
 import org.gradle.api.JavaVersion
 
-object RudderstackBuildConfig {
+object RudderStackBuildConfig {
 
     object Build {
+
         val JAVA_VERSION = JavaVersion.VERSION_17
         val JVM_TARGET = "17"
     }
 
     object Android {
-        val COMPILE_SDK =x
+
+        val COMPILE_SDK = 34
         val MIN_SDK = 19
         val TARGET_SDK = COMPILE_SDK
     }
 
     object Version {
-        val VERSION_NAME = "\"2.0\""
+
+        val VERSION_NAME = "\"1.0.0\""
+        val VERSION_CODE = "\"1\""
     }
 
     object Kotlin {
+
         val COMPILER_EXTENSION_VERSION = "1.4.8"
     }
 
     object ReleaseInfo {
+
         val VERSION_NAME = ""
         val GROUP_NAME = ""
     }
