3942329527c16396f2208de5e4b0e1b72c6d2695
Chryssa Aliferi <c.aliferi@gmail.com>
2024-09-10 18:48:14 +0300

    docs: removes documentation from QueuedPayLoadTooLargeException

--- android/src/main/kotlin/storage/exceptions/QueuedPayloadTooLargeException.kt
+++ android/src/main/kotlin/storage/exceptions/QueuedPayloadTooLargeException.kt
@@ -1,15 +1,5 @@
 package com.rudderstack.android.storage.exceptions
 
-/**
- * `InvalidConfigurationException` is a custom exception that is thrown when an invalid
- * configuration is detected in the application.
- *
- * This exception provides additional context about the configuration error, allowing developers
- * to understand what went wrong during the setup or runtime.
- *
- * @param message A detailed message explaining the cause of the exception.
- * @param cause The original exception that caused this exception to be thrown (optional).
- */
 internal class QueuedPayloadTooLargeException(
     message: String,
     cause: Throwable? = null
