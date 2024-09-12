5d1b59d8ced4cca78739ade982570d8e76e49272
Chryssa Aliferi <c.aliferi@gmail.com>
2024-09-12 20:46:09 +0300

    feat: create SourceConfigStateTest.kt

---
+++ core/src/test/kotlin/com/rudderstack/core/state/SourceConfigStateTest.kt
@@ -0,0 +1,83 @@
+package com.rudderstack.core.state
+
+import com.rudderstack.core.internals.models.RudderServerConfigSource
+import com.rudderstack.core.internals.models.SourceConfig
+import com.rudderstack.core.internals.storage.Storage
+import com.rudderstack.core.internals.storage.StorageKeys.SOURCE_CONFIG_PAYLOAD
+import com.rudderstack.core.internals.storage.StorageKeys.SOURCE_IS_ENABLED
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import kotlinx.serialization.json.Json
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SourceConfigStateTest {
+
+    private val storage: Storage = mockk(relaxed = true)
+    private val testScope = TestScope(UnconfinedTestDispatcher())
+    private val initialState = SourceConfigState.initialState()
+    private val reducer = SourceConfigState.SaveSourceConfigValues(storage, testScope)
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `given an initial SourceConfigState when state is updated then update subscribers with updated SourceConfig`() {
+        val newSourceConfig = SourceConfig(
+            source = RudderServerConfigSource(
+                sourceId = "newId",
+                sourceName = "newName",
+                writeKey = "newKey",
+                isSourceEnabled = true,
+                workspaceId = "newWorkspaceId",
+                updatedAt = "2023-09-12"
+            )
+        )
+
+        val updateAction = SourceConfigState.Update(newSourceConfig)
+        val newState = reducer.invoke(initialState, updateAction)
+
+        assertEquals(newSourceConfig, newState.sourceConfig)
+    }
+
+    @Test
+    fun `given an initial SourceConfigState, when state is updated then save the result in the storage`() = runTest {
+        val newSourceConfig = SourceConfig(
+            source = RudderServerConfigSource(
+                sourceId = "newId",
+                sourceName = "newName",
+                writeKey = "newKey",
+                isSourceEnabled = true,
+                workspaceId = "newWorkspaceId",
+                updatedAt = "2023-09-12"
+            )
+        )
+
+        val updateAction = SourceConfigState.Update(newSourceConfig)
+        reducer.invoke(initialState, updateAction)
+        advanceUntilIdle()
+
+        coVerify {
+            storage.write(SOURCE_CONFIG_PAYLOAD, Json.encodeToString(SourceConfig.serializer(), newSourceConfig))
+            storage.write(SOURCE_IS_ENABLED, newSourceConfig.source.isSourceEnabled)
+        }
+    }
+}
