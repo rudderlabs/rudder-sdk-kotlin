bd027827065bbc8ef33d255f10920e68b70abf53
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-09-16 20:19:55 +0530

    test: add test cases for track event

---
+++ core/src/test/kotlin/com/rudderstack/core/internals/models/provider/MessageProvider.kt
@@ -0,0 +1,40 @@
+package com.rudderstack.core.internals.models.provider
+
+
+import com.rudderstack.core.internals.models.emptyJsonObject
+import kotlinx.serialization.json.add
+import kotlinx.serialization.json.buildJsonArray
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+
+fun provideSampleJsonPayload() = buildJsonObject {
+    put("key-1", "String value")
+    put("key-2", 123)
+    put("key-3", true)
+    put("key-4", 123.456)
+    put("key-5", buildJsonObject {
+        put("key-6", "String value")
+        put("key-7", 123)
+        put("key-8", true)
+        put("key-9", 123.456)
+    })
+    put("key-10", buildJsonArray {
+        add("String value")
+        add(123)
+        add(true)
+        add(123.456)
+    })
+    put("key-11", emptyJsonObject)
+}
+
+fun provideSampleIntegrationsPayload() = mapOf(
+    "Amplitude" to true,
+    "All" to false,
+    "Firebase" to true,
+    "Braze" to false,
+)
+
+fun provideSampleExternalIdsPayload() = listOf(
+    mapOf("key-1" to "value-1"),
+    mapOf("key-2" to "value-2"),
+)
