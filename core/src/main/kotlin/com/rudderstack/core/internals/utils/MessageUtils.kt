ed2d58662f0cf5961c3c6cbf244ff01eeded0e9f
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-09-26 14:19:58 +0530

    refactor: make name and category private constant fields

--- core/src/main/kotlin/com/rudderstack/core/internals/utils/MessageUtils.kt
+++ core/src/main/kotlin/com/rudderstack/core/internals/utils/MessageUtils.kt
@@ -4,14 +4,17 @@ import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 
+private const val NAME = "name"
+private const val CATEGORY = "category"
+
 internal fun addNameAndCategoryToProperties(name: String, category: String, properties: JsonObject): JsonObject {
     val nameAndCategoryProperties: JsonObject =
         buildJsonObject {
             if (name.isNotEmpty()) {
-                put("name", name)
+                put(NAME, name)
             }
             if (category.isNotEmpty()) {
-                put("category", category)
+                put(CATEGORY, category)
             }
         }
 
