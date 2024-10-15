e380cf5e8a79b4d438ccc649ff5a1dcf13c860d8
Vishal Gupta <vishal@rudderstack.com>
2024-10-15 14:25:34 +0530

    refactor: rename trackNavigationDestinations to setNavigationDestinations

--- android/src/main/kotlin/com/rudderstack/android/sdk/Analytics.kt
+++ android/src/main/kotlin/com/rudderstack/android/sdk/Analytics.kt
@@ -86,7 +86,7 @@ class Analytics(
      * fun SunflowerApp() {
      *     val navController = rememberNavController()
      *     LaunchedEffect("first_launch") {
-     *         analytics.trackNavigationDestinations(navController, this@MainActivity)
+     *         analytics.setNavigationDestinations(navController, this@MainActivity)
      *     }
      *
      *     SunFlowerNavHost(
@@ -109,7 +109,7 @@ class Analytics(
      *             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
      *         // Instantiate the navController using the NavHostFragment
      *         navController = navHostFragment.navController
-     *         analytics.trackNavigationDestinations(navController, this@MainActivity)
+     *         analytics.setNavigationDestinations(navController, this@MainActivity)
      *     }
      * }
      * ```
@@ -120,7 +120,7 @@ class Analytics(
      */
     @Synchronized
     @Experimental
-    fun trackNavigationDestinations(navController: NavController, activity: Activity) {
+    fun setNavigationDestinations(navController: NavController, activity: Activity) {
         if (navControllerTrackingPlugin == null) {
             navControllerTrackingPlugin = NavControllerTrackingPlugin(navContextStore).also {
                 add(it)
