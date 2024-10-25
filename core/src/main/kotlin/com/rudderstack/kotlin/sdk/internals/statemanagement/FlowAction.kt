de716b6bebd1d627c3c301221889c9d23536cedf
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-10-25 13:35:36 +0530

    chore: remove unused classes

--- core/src/main/kotlin/com/rudderstack/kotlin/sdk/internals/statemanagement/FlowAction.kt
+++
@@ -1,7 +0,0 @@
-@file:Suppress("UndocumentedPublicFunction", "UndocumentedPublicClass")
-
-package com.rudderstack.kotlin.sdk.internals.statemanagement
-
-fun interface FlowAction<T> {
-    suspend fun reduce(currentState: T): T
-}
