500cd1db0708f2d6de52c715e8cc834beb391b1c
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-10-31 03:38:15 +0530

    chore: use LenientJson in StorageUtils

--- core/src/main/kotlin/com/rudderstack/kotlin/sdk/internals/utils/StorageUtils.kt
+++ core/src/main/kotlin/com/rudderstack/kotlin/sdk/internals/utils/StorageUtils.kt
@@ -4,7 +4,6 @@ import com.rudderstack.kotlin.sdk.internals.models.ExternalId
 import com.rudderstack.kotlin.sdk.internals.models.emptyJsonObject
 import com.rudderstack.kotlin.sdk.internals.storage.Storage
 import com.rudderstack.kotlin.sdk.internals.storage.StorageKeys
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.decodeFromJsonElement
 import kotlinx.serialization.json.jsonArray
@@ -23,7 +22,7 @@ internal fun Storage.readExternalIdAndDecodeOrDefault(key: StorageKeys): List<Ex
     readString(key = key, defaultVal = String.empty()).let {
         if (it.isNotEmpty()) {
             // Parse the JSON string as a JsonObject
-            val jsonObject = Json.parseToJsonElement(it).jsonObject
+            val jsonObject = LenientJson.parseToJsonElement(it).jsonObject
             // Get the JsonArray corresponding to "externalId" and deserialize it to a List<ExternalIds>
             val externalIdsArray = jsonObject["externalId"]?.jsonArray ?: JsonArray(emptyList())
             LenientJson.decodeFromJsonElement(externalIdsArray)
