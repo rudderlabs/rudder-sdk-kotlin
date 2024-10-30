7d610d6a5364bee5f8142d9b7a4db943b55139f1
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-10-31 02:43:27 +0530

    test: add test cases for ListUtils

--- core/src/main/kotlin/com/rudderstack/kotlin/sdk/internals/utils/ListUtils.kt
+++ core/src/main/kotlin/com/rudderstack/kotlin/sdk/internals/utils/ListUtils.kt
@@ -2,7 +2,6 @@ package com.rudderstack.kotlin.sdk.internals.utils
 
 import com.rudderstack.kotlin.sdk.internals.models.ExternalId
 
-// TODO: Add unit test cases
 internal infix fun List<ExternalId>.mergeWithHigherPriorityTo(other: List<ExternalId>): List<ExternalId> {
     val mergedList = this + other
     return mergedList.associateBy { it.type }.values.toList()
