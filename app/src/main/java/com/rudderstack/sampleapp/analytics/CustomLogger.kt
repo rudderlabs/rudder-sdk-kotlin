28d4070e345b56c473d1711e59c89cecf13247d8
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-11-05 12:29:27 +0530

    chore: rename CustomLogger TAG

--- app/src/main/java/com/rudderstack/sampleapp/analytics/CustomLogger.kt
+++ app/src/main/java/com/rudderstack/sampleapp/analytics/CustomLogger.kt
@@ -3,28 +3,28 @@ package com.rudderstack.sampleapp.analytics
 import android.util.Log
 import com.rudderstack.kotlin.sdk.internals.logger.Logger
 
-var CHRYSSA_TAG = "Chryssa-Analytics"
+var CUSTOM_TAG = "Custom-Analytics"
     private set
 
 internal class CustomLogger : Logger {
 
     override fun verbose(log: String) {
-        Log.v(CHRYSSA_TAG, log)
+        Log.v(CUSTOM_TAG, log)
     }
 
     override fun debug(log: String) {
-        Log.d(CHRYSSA_TAG, log)
+        Log.d(CUSTOM_TAG, log)
     }
 
     override fun info(log: String) {
-        Log.i(CHRYSSA_TAG, log)
+        Log.i(CUSTOM_TAG, log)
     }
 
     override fun warn(log: String) {
-        Log.w(CHRYSSA_TAG, log)
+        Log.w(CUSTOM_TAG, log)
     }
 
     override fun error(log: String, throwable: Throwable?) {
-        Log.e(CHRYSSA_TAG, log, throwable)
+        Log.e(CUSTOM_TAG, log, throwable)
     }
 }
