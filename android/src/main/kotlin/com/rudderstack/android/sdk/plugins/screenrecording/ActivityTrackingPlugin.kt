83d717d161746c1fc6719fbae6b2f2364d3ae4b3
Vishal Gupta <vishal@rudderstack.com>
2024-11-07 10:25:18 +0530

    refactor: refactor deeplink tracking plugin to use ActivityLifecycleObserver

--- android/src/main/kotlin/com/rudderstack/android/sdk/plugins/screenrecording/ActivityTrackingPlugin.kt
+++ android/src/main/kotlin/com/rudderstack/android/sdk/plugins/screenrecording/ActivityTrackingPlugin.kt
@@ -1,72 +1,32 @@
 package com.rudderstack.android.sdk.plugins.screenrecording
 
 import android.app.Activity
-import android.app.Application
-import android.os.Bundle
 import com.rudderstack.android.sdk.Configuration
+import com.rudderstack.android.sdk.plugins.lifecyclemanagment.ActivityLifecycleObserver
+import com.rudderstack.android.sdk.utils.addLifecycleObserver
 import com.rudderstack.android.sdk.utils.automaticProperty
 import com.rudderstack.kotlin.sdk.Analytics
 import com.rudderstack.kotlin.sdk.internals.plugins.Plugin
-import kotlin.properties.Delegates
+import com.rudderstack.android.sdk.Analytics as AndroidAnalytics
 
 // plugin to track activity screen events.
-internal class ActivityTrackingPlugin : Plugin, Application.ActivityLifecycleCallbacks {
+internal class ActivityTrackingPlugin : Plugin, ActivityLifecycleObserver {
 
     override val pluginType: Plugin.PluginType = Plugin.PluginType.Manual
 
     override lateinit var analytics: Analytics
 
-    private lateinit var application: Application
-    private var trackActivities by Delegates.notNull<Boolean>()
-
     override fun setup(analytics: Analytics) {
         super.setup(analytics)
 
         (analytics.configuration as? Configuration)?.let { config ->
-            trackActivities = config.trackActivities
-            if (trackActivities) {
-                application = config.application
-                application.registerActivityLifecycleCallbacks(this)
+            if (config.trackActivities) {
+                (analytics as? AndroidAnalytics)?.addLifecycleObserver(this)
             }
         }
     }
 
-    override fun teardown() {
-        super.teardown()
-        if (trackActivities) {
-            application.unregisterActivityLifecycleCallbacks(this)
-        }
-    }
-
-    override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
-        // NO-OP
-    }
-
     override fun onActivityStarted(activity: Activity) {
-        trackActivity(activity)
-    }
-
-    override fun onActivityResumed(activity: Activity) {
-        // NO-OP
-    }
-
-    override fun onActivityPaused(activity: Activity) {
-        // NO-OP
-    }
-
-    override fun onActivityStopped(activity: Activity) {
-        // NO-OP
-    }
-
-    override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) {
-        // NO-OP
-    }
-
-    override fun onActivityDestroyed(activity: Activity) {
-        // NO-OP
-    }
-
-    private fun trackActivity(activity: Activity) {
         analytics.screen(screenName = activity.localClassName, properties = automaticProperty())
     }
 }
