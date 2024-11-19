d5888433aaa494ecb5523a97e1543217b8d5139b
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-11-19 18:35:07 +0530

    chore: intentionally remove doc to test linting tools

--- core/src/main/kotlin/com/rudderstack/kotlin/sdk/internals/models/RudderOption.kt
+++ core/src/main/kotlin/com/rudderstack/kotlin/sdk/internals/models/RudderOption.kt
@@ -4,13 +4,6 @@ import com.rudderstack.kotlin.sdk.internals.utils.DEFAULT_INTEGRATIONS
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
 
-/**
- * Represents options that can be passed with a message in the RudderStack analytics context.
- *
- * @property integrations A map of integration names to a boolean indicating if they are enabled.
- * @property externalIds A list of maps representing external IDs associated with the message.
- * @property customContext A JSON object representing custom contexts associated with the message.
- */
 data class RudderOption(
     val integrations: Map<String, Boolean> = DEFAULT_INTEGRATIONS,
     val externalIds: List<ExternalId> = emptyList(),
