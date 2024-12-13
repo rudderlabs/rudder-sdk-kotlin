784367c58085ac37a851a6a0778889e2ec52fb05
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-12-14 01:50:40 +0530

    chore(android): prepare android for publishing

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -64,6 +64,39 @@ android {
     }
 }
 
+// For generating SourcesJar and JavadocJar
+tasks {
+    val sourceFiles = (android.sourceSets["main"].kotlin as com.android.build.gradle.internal.api.DefaultAndroidSourceDirectorySet).srcDirs
+
+    register<Javadoc>("withJavadoc") {
+        isFailOnError = false
+
+        setSource(sourceFiles)
+
+        // add Android runtime classpath
+        android.bootClasspath.forEach { classpath += project.fileTree(it) }
+
+        // add classpath for all dependencies
+        android.libraryVariants.forEach { variant ->
+            variant.javaCompileProvider.get().classpath.files.forEach { file ->
+                classpath += project.fileTree(file)
+            }
+        }
+    }
+
+    register<Jar>("javadocJar") {
+        archiveClassifier.set("javadoc")
+        dependsOn(named("withJavadoc"))
+        val destination = named<Javadoc>("withJavadoc").get().destinationDir
+        from(destination)
+    }
+
+    register<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceFiles)
+    }
+}
+
 dependencies {
     //api
     api(project(":core"))
@@ -86,3 +119,5 @@ dependencies {
     testImplementation(libs.json.assert)
     testImplementation(libs.navigation.runtime)
 }
+
+apply(from = rootProject.file("gradle/publishing/publishing.gradle.kts"))
