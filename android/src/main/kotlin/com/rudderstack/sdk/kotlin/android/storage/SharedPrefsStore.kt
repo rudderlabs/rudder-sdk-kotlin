f0d43e6b1ff6ff8188d08150bc6396216bd2852b
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-01-02 18:35:54 +0530

    chore(android): remove stale delete from SharedPrefs
    Currently, this method is not utilised.

--- android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/storage/SharedPrefsStore.kt
+++ android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/storage/SharedPrefsStore.kt
@@ -3,16 +3,15 @@ package com.rudderstack.sdk.kotlin.android.storage
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.core.content.edit
+import com.rudderstack.sdk.kotlin.core.internals.logger.LoggerAnalytics
 import com.rudderstack.sdk.kotlin.core.internals.storage.KeyValueStorage
-import java.io.File
 
 internal class SharedPrefsStore(
-    private val context: Context,
+    context: Context,
     prefsName: String,
 ) : KeyValueStorage {
 
     private val preferences: SharedPreferences = context.getSharedPreferences(prefsName, Context.MODE_PRIVATE)
-    private val useCase: CheckBuildVersionUseCase = CheckBuildVersionUseCase()
 
     override fun getInt(key: String, defaultVal: Int): Int {
         return preferences.getInt(key, defaultVal)
@@ -50,16 +49,6 @@ internal class SharedPrefsStore(
         with(preferences.edit()) {
             remove(key)
             commit()
-        }
-    }
-
-    private fun delete(fileName: String) {
-        if (useCase.isAndroidVersionNougatAndAbove()) {
-            context.deleteSharedPreferences(fileName)
-        } else {
-            context.getSharedPreferences(fileName, Context.MODE_PRIVATE).edit().clear().apply()
-            val dir = File(context.applicationInfo.dataDir, "shared_prefs")
-            File(dir, "$fileName.xml").delete()
         }
     }
 
