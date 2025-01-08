e0e6d832440329f97761ddf6ff58b851f8d20f59
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-01-08 21:36:30 +0530

    feat: add ConnectivityObserver interface

---
+++ core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/connectivity/ConnectivityObserver.kt
@@ -0,0 +1,33 @@
+package com.rudderstack.sdk.kotlin.core.internals.connectivity
+
+import com.rudderstack.sdk.kotlin.core.internals.utils.InternalRudderApi
+
+/**
+ * Interface for observing network availability.
+ */
+@InternalRudderApi
+interface ConnectivityObserver : ConnectivitySubscriber
+
+/**
+ * Base class for connectivity observers.
+ */
+@InternalRudderApi
+abstract class BaseConnectivityObserver : ConnectivitySubscriber {
+
+    /**
+     * The connectivity observer to be used.
+     */
+    protected abstract val connectivityObserver: ConnectivityObserver
+
+    /**
+     * Observes the network availability and notifies the subscriber immediately if the network is available.
+     * Otherwise, it waits for the network to be available and then notifies the subscriber.
+     *
+     * **NOTE**: Subscriber are notified exactly once.
+     *
+     * @param subscriber The subscriber to be notified when the network is available.
+     */
+    override suspend fun notifyImmediatelyOrSubscribe(subscriber: suspend () -> Unit) {
+        connectivityObserver.notifyImmediatelyOrSubscribe(subscriber)
+    }
+}
