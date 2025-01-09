67bd139577d57493fd563a73ba55771896fd979a
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-01-09 14:48:04 +0530

    test(core): fix DefaultConnectivityObserverTest

--- core/src/test/kotlin/com/rudderstack/sdk/kotlin/core/internals/connectivity/DefaultConnectivityObserverTest.kt
+++ core/src/test/kotlin/com/rudderstack/sdk/kotlin/core/internals/connectivity/DefaultConnectivityObserverTest.kt
@@ -13,12 +13,10 @@ class DefaultConnectivityObserverTest {
         val subscriber2 = provideSpyBlock()
         val defaultObserver = DefaultConnectivityObserver()
 
-        defaultObserver.immediateNotifyOrObserveConnectivity {
-            subscriber1.execute()
-            defaultObserver.immediateNotifyOrObserveConnectivity { subscriber2.execute() }
+        defaultObserver.immediateNotifyOrObserveConnectivity { subscriber1.execute() }
+        defaultObserver.immediateNotifyOrObserveConnectivity { subscriber2.execute() }
 
-            coVerify { subscriber1.execute() }
-            coVerify { subscriber2.execute() }
-        }
+        coVerify { subscriber1.execute() }
+        coVerify { subscriber2.execute() }
     }
 }
