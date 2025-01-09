d268e9b5a3655634abf50eea1fe2a4f3760b4e4d
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-01-09 14:29:29 +0530

    test(core): refactor DefaultConnectivityObserverTest.kt

--- core/src/test/kotlin/com/rudderstack/sdk/kotlin/core/internals/connectivity/DefaultConnectivityObserverTest.kt
+++ core/src/test/kotlin/com/rudderstack/sdk/kotlin/core/internals/connectivity/DefaultConnectivityObserverTest.kt
@@ -1,7 +1,7 @@
 package com.rudderstack.sdk.kotlin.core.internals.connectivity
 
+import com.rudderstack.sdk.kotlin.core.provideSpyBlock
 import io.mockk.coVerify
-import io.mockk.spyk
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
@@ -9,26 +9,16 @@ class DefaultConnectivityObserverTest {
 
     @Test
     fun `when subscriber is added, then they are notified immediately`() = runTest {
-        val subscriber1 = createSpySubscriber()
-        val subscriber2 = createSpySubscriber()
+        val subscriber1 = provideSpyBlock()
+        val subscriber2 = provideSpyBlock()
         val defaultObserver = DefaultConnectivityObserver()
 
-        defaultObserver.notifyImmediatelyOrSubscribe { subscriber1.subscribe() }
-        defaultObserver.notifyImmediatelyOrSubscribe { subscriber2.subscribe() }
+        defaultObserver.notifyImmediatelyOrSubscribe {
+            subscriber1.execute()
+            defaultObserver.notifyImmediatelyOrSubscribe { subscriber2.execute() }
 
-        coVerify { subscriber1.subscribe() }
-        coVerify { subscriber2.subscribe() }
-    }
-
-    private fun createSpySubscriber(): NetworkObserver {
-        return spyk(NetworkObserver())
-    }
-}
-
-// As Mockk doesn't support spying on suspend lambda function, we need to create a class for the same.
-private class NetworkObserver {
-
-    fun subscribe() {
-        // Do nothing
+            coVerify { subscriber1.execute() }
+            coVerify { subscriber2.execute() }
+        }
     }
 }
