482285d1b19f95d6631fe323784d342d96bd9f09
Vishal Gupta <vishal@rudderstack.com>
2025-01-11 17:50:55 +0530

    docs: add docs for PluginChain

--- core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/plugins/PluginChain.kt
+++ core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/plugins/PluginChain.kt
@@ -5,6 +5,9 @@ import com.rudderstack.sdk.kotlin.core.internals.models.Event
 import com.rudderstack.sdk.kotlin.core.internals.utils.InternalRudderApi
 import kotlin.reflect.KClass
 
+/**
+ * PluginChain manages all the different types of plugins.
+ */
 @InternalRudderApi
 class PluginChain(
     private val pluginList: Map<Plugin.PluginType, PluginInteractor> = mapOf(
@@ -16,19 +19,31 @@ class PluginChain(
     )
 ) {
 
+    /**
+     * The analytics instance that the plugins are attached to.
+     */
     lateinit var analytics: Analytics
 
+    /**
+     * Processes an event through the plugin chain.
+     */
     suspend fun process(event: Event) {
         val preProcessResult = applyPlugins(Plugin.PluginType.PreProcess, event)
         val onProcessResult = applyPlugins(Plugin.PluginType.OnProcess, preProcessResult)
         applyPlugins(Plugin.PluginType.Destination, onProcessResult)
     }
 
+    /**
+     * Adds a plugin to the plugin chain.
+     */
     fun add(plugin: Plugin) {
         plugin.setup(analytics)
         pluginList[plugin.pluginType]?.add(plugin)
     }
 
+    /**
+     * Removes a plugin from the plugin chain.
+     */
     fun remove(plugin: Plugin) {
         // remove all plugins with this name in every category
         pluginList.forEach { (_, list) ->
@@ -39,12 +54,18 @@ class PluginChain(
         }
     }
 
+    /**
+     * Applies a closure to all plugins in the plugin chain.
+     */
     fun applyClosure(closure: (Plugin) -> Unit) {
         pluginList.forEach { (_, mediator) ->
             mediator.applyClosure(closure)
         }
     }
 
+    /**
+     * Finds all plugins of the given class and type in the plugin chain.
+     */
     fun <T : Plugin> findAll(pluginType: Plugin.PluginType, pluginClass: KClass<T>): List<T> {
         val result = mutableListOf<T>()
 
@@ -54,6 +75,9 @@ class PluginChain(
         return result
     }
 
+    /**
+     * Finds a plugin of the given class in the plugin chain.
+     */
     fun removeAll() {
         applyClosure { it.teardown() }
         pluginList.forEach { (_, mediator) ->
@@ -61,6 +85,9 @@ class PluginChain(
         }
     }
 
+    /**
+     * Executes an event through all the plugins of a plugin type.
+     */
     suspend fun applyPlugins(pluginType: Plugin.PluginType, event: Event?): Event? {
         var result: Event? = event
         val mediator = pluginList[pluginType]
