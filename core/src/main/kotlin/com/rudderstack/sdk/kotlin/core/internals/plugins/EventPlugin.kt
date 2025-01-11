9be1aaace0a311b79fa70d06ffc9980be8604a30
Vishal Gupta <vishal@rudderstack.com>
2025-01-11 17:52:05 +0530

    docs: add docs for EventPlugin

--- core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/plugins/EventPlugin.kt
+++ core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/plugins/EventPlugin.kt
@@ -8,31 +8,52 @@ import com.rudderstack.sdk.kotlin.core.internals.models.ScreenEvent
 import com.rudderstack.sdk.kotlin.core.internals.models.TrackEvent
 import com.rudderstack.sdk.kotlin.core.internals.utils.InternalRudderApi
 
+/**
+ * Advanced plugin that can act on specific event payloads.
+ */
 @InternalRudderApi
 interface EventPlugin : Plugin {
 
+    /**
+     * Handles a track event.
+     */
     fun track(payload: TrackEvent): Event? {
         return payload
     }
 
+    /**
+     * Handles a screen event.
+     */
     fun screen(payload: ScreenEvent): Event? {
         return payload
     }
 
+    /**
+     * Handles a group event.
+     */
     fun group(payload: GroupEvent): Event? {
         return payload
     }
 
+    /**
+     * Handles an identify event.
+     */
     fun identify(payload: IdentifyEvent): Event? {
         return payload
     }
 
+    /**
+     * Handles an alias event.
+     */
     fun alias(payload: AliasEvent): Event? {
         return payload
     }
 
     override suspend fun intercept(event: Event): Event? = handleEvent(event)
 
+    /**
+     * Executes the appropriate method for the given event.
+     */
     fun handleEvent(event: Event): Event? = when (event) {
         is TrackEvent -> track(event)
         is ScreenEvent -> screen(event)
