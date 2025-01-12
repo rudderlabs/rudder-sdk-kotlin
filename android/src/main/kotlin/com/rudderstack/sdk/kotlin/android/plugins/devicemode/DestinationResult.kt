3b7451badd3bb6e6b6825f522d550f20f7ae3dab
Vishal Gupta <vishal@rudderstack.com>
2025-01-12 17:44:21 +0530

    docs: add docs for DestinationResult

--- android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/plugins/devicemode/DestinationResult.kt
+++ android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/plugins/devicemode/DestinationResult.kt
@@ -2,4 +2,7 @@ package com.rudderstack.sdk.kotlin.android.plugins.devicemode
 
 import com.rudderstack.sdk.kotlin.core.internals.utils.Result
 
+/**
+ * The result of a destination initialisation returned in `onDestinationReady` callback.
+ */
 typealias DestinationResult = Result<Unit, Exception>
