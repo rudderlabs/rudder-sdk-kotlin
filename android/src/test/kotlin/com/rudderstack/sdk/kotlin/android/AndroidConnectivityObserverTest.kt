5d4b1d1076f0d1303f59011566d480370acac720
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-01-14 21:51:12 +0530

    test: add AndroidConnectivityObserverTest.kt

---
+++ android/src/test/kotlin/com/rudderstack/sdk/kotlin/android/AndroidConnectivityObserverTest.kt
@@ -0,0 +1,226 @@
+package com.rudderstack.sdk.kotlin.android
+
+import android.app.Application
+import android.content.BroadcastReceiver
+import android.net.ConnectivityManager
+import android.os.Build
+import com.rudderstack.sdk.kotlin.android.connectivity.AndroidConnectivityObserverPlugin
+import com.rudderstack.sdk.kotlin.android.connectivity.createBroadcastReceiver
+import com.rudderstack.sdk.kotlin.android.connectivity.createNetworkCallback
+import com.rudderstack.sdk.kotlin.android.utils.AppSDKVersion
+import com.rudderstack.sdk.kotlin.android.utils.application
+import com.rudderstack.sdk.kotlin.core.Analytics
+import com.rudderstack.sdk.kotlin.core.internals.models.connectivity.ConnectivityState
+import com.rudderstack.sdk.kotlin.core.internals.statemanagement.FlowState
+import io.mockk.CapturingSlot
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkObject
+import io.mockk.mockkStatic
+import io.mockk.slot
+import io.mockk.unmockkStatic
+import io.mockk.verify
+import junit.framework.TestCase.assertTrue
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+private const val COMPATIBLE_SDK_VERSION = Build.VERSION_CODES.N // 24
+private const val LEGACY_SDK_VERSION = Build.VERSION_CODES.LOLLIPOP // 21
+
+class AndroidConnectivityObserverTest {
+
+    @MockK
+    private lateinit var mockAnalytics: Analytics
+
+    @MockK
+    private lateinit var mockApplication: Application
+
+    @MockK
+    private lateinit var mockNetworkCallback: ConnectivityManager.NetworkCallback
+
+    @MockK
+    private lateinit var mockBroadcastReceiver: BroadcastReceiver
+
+    @MockK
+    private lateinit var mockConnectivityManager: ConnectivityManager
+
+    @MockK
+    private lateinit var mockConnectivityState: FlowState<Boolean>
+
+    private lateinit var connectivityStateSlot: CapturingSlot<FlowState<Boolean>>
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this, relaxed = true)
+
+        every { mockAnalytics.application } returns mockApplication
+        every { mockApplication.getSystemService(ConnectivityManager::class.java) } returns mockConnectivityManager
+
+        connectivityStateSlot = slot()
+        mockkStatic(::createBroadcastReceiver, ::createNetworkCallback)
+        every {
+            createNetworkCallback(
+                connectivityState = capture(connectivityStateSlot),
+            )
+        } returns mockNetworkCallback
+        every {
+            createBroadcastReceiver(
+                connectivityState = capture(connectivityStateSlot),
+            )
+        } returns mockBroadcastReceiver
+
+        mockkObject(AppSDKVersion)
+    }
+
+    @After
+    fun tearDown() {
+        unmockkStatic(::createNetworkCallback, ::createBroadcastReceiver)
+    }
+
+    @Test
+    fun `given compatible sdk version, when connection gets available, then connection state is enabled`() {
+        every { AppSDKVersion.getVersionSDKInt() } returns COMPATIBLE_SDK_VERSION
+        provideAndroidConnectivityObserverPlugin()
+
+        simulateNetworkAvailability()
+
+        verifyConnectivityStateEnabled()
+    }
+
+    @Test
+    fun `given legacy sdk version, when connection gets available, then connection state is enabled`() {
+        every { AppSDKVersion.getVersionSDKInt() } returns LEGACY_SDK_VERSION
+        provideAndroidConnectivityObserverPlugin()
+
+        simulateNetworkAvailability()
+
+        verifyConnectivityStateEnabled()
+    }
+
+    @Test
+    fun `given compatible sdk version, when connection gets unavailable, then connection state is enabled`() {
+        every { AppSDKVersion.getVersionSDKInt() } returns COMPATIBLE_SDK_VERSION
+        provideAndroidConnectivityObserverPlugin()
+
+        simulateNetworkUnAvailability()
+
+        verifyConnectivityStateDisabled()
+    }
+
+    @Test
+    fun `given legacy sdk version, when connection gets unavailable, then connection state is enabled`() {
+        every { AppSDKVersion.getVersionSDKInt() } returns LEGACY_SDK_VERSION
+        provideAndroidConnectivityObserverPlugin()
+
+        simulateNetworkUnAvailability()
+
+        verifyConnectivityStateDisabled()
+    }
+
+    // Note: Legacy SDK `register` API doesn't throws a runtime exception when registering for network callback.
+    @Test
+    fun `given compatible SDK version and runtime exception occurs while registering for network callback, when registering connectivity observer, then connection state should be enabled`() {
+        every { AppSDKVersion.getVersionSDKInt() } returns COMPATIBLE_SDK_VERSION
+        every { mockConnectivityManager.registerDefaultNetworkCallback(any()) } throws RuntimeException()
+
+        provideAndroidConnectivityObserverPlugin()
+
+        verifyDefaultConnectivityState()
+    }
+
+    @Test
+    fun `given compatible SDK version, when teardown is called, then network callback is unregistered`() {
+        every { AppSDKVersion.getVersionSDKInt() } returns COMPATIBLE_SDK_VERSION
+        val plugin = provideAndroidConnectivityObserverPlugin()
+
+        plugin.teardown()
+
+        verify(exactly = 1) { mockConnectivityManager.unregisterNetworkCallback(mockNetworkCallback) }
+    }
+
+    @Test
+    fun `given legacy SDK version, when teardown is called, then broadcast receiver is unregistered`() {
+        every { AppSDKVersion.getVersionSDKInt() } returns LEGACY_SDK_VERSION
+        val plugin = provideAndroidConnectivityObserverPlugin()
+
+        plugin.teardown()
+
+        verify(exactly = 1) { mockApplication.unregisterReceiver(mockBroadcastReceiver) }
+    }
+
+    private fun provideAndroidConnectivityObserverPlugin() = AndroidConnectivityObserverPlugin(
+        connectivityState = mockConnectivityState,
+    ).also { it.setup(mockAnalytics) }
+
+    /**
+     * Simulates both scenarios: network availability prior to and after SDK initialization.
+     *
+     * The [ConnectivityManager.NetworkCallback] `onAvailable` method is invoked whether the network is initially available or becomes available during runtime.
+     */
+    private fun simulateNetworkAvailability() {
+        connectivityStateSlot.captured.dispatch(ConnectivityState.EnableConnectivityAction())
+    }
+
+    private fun simulateNetworkUnAvailability() {
+        connectivityStateSlot.captured.dispatch(ConnectivityState.DisableConnectivityAction())
+    }
+
+    private fun verifyConnectivityStateEnabled() {
+        assertTrue(connectivityStateSlot.isCaptured)
+        verify(exactly = 1) {
+            mockConnectivityState.dispatch(
+                match { it is ConnectivityState.EnableConnectivityAction }
+            )
+        }
+        verify(exactly = 0) {
+            mockConnectivityState.dispatch(
+                match { it is ConnectivityState.DisableConnectivityAction }
+            )
+        }
+        verify(exactly = 0) {
+            mockConnectivityState.dispatch(
+                match { it is ConnectivityState.SetDefaultStateAction }
+            )
+        }
+    }
+
+    private fun verifyConnectivityStateDisabled() {
+        assertTrue(connectivityStateSlot.isCaptured)
+        verify(exactly = 1) {
+            mockConnectivityState.dispatch(
+                match { it is ConnectivityState.DisableConnectivityAction }
+            )
+        }
+        verify(exactly = 0) {
+            mockConnectivityState.dispatch(
+                match { it is ConnectivityState.EnableConnectivityAction }
+            )
+        }
+        verify(exactly = 0) {
+            mockConnectivityState.dispatch(
+                match { it is ConnectivityState.SetDefaultStateAction }
+            )
+        }
+    }
+
+    private fun verifyDefaultConnectivityState() {
+        assertTrue(connectivityStateSlot.isCaptured)
+        verify(exactly = 1) {
+            mockConnectivityState.dispatch(
+                match { it is ConnectivityState.SetDefaultStateAction }
+            )
+        }
+        verify(exactly = 0) {
+            mockConnectivityState.dispatch(
+                match { it is ConnectivityState.EnableConnectivityAction }
+            )
+        }
+        verify(exactly = 0) {
+            mockConnectivityState.dispatch(
+                match { it is ConnectivityState.DisableConnectivityAction }
+            )
+        }
+    }
+}
