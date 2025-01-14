a6c5959577f5530bd5d3ae5196010e18a3a5838c
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-01-14 18:39:56 +0530

    feat: add ConnectivityState.kt

---
+++ core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/connectivity/ConnectivityState.kt
@@ -0,0 +1,41 @@
+package com.rudderstack.sdk.kotlin.core.internals.connectivity
+
+import com.rudderstack.sdk.kotlin.core.internals.statemanagement.FlowAction
+import com.rudderstack.sdk.kotlin.core.internals.utils.InternalRudderApi
+
+private const val DEFAULT_STATE = true
+
+/**
+ * State management for connectivity.
+ */
+@InternalRudderApi
+class ConnectivityState {
+
+    companion object {
+
+        internal const val INITIAL_STATE = false
+    }
+
+    /**
+     * Action to set the default state of connectivity.
+     * Default state is always `true`.
+     */
+    class SetDefaultStateAction : FlowAction<Boolean> {
+
+        override fun reduce(currentState: Boolean): Boolean {
+            return DEFAULT_STATE
+        }
+    }
+
+    /**
+     * Action to toggle the current state of connectivity.
+     *
+     * @param newState The new state of connectivity.
+     */
+    class ToggleStateAction(private val newState: Boolean) : FlowAction<Boolean> {
+
+        override fun reduce(currentState: Boolean): Boolean {
+            return newState
+        }
+    }
+}
