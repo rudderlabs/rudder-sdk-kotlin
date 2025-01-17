e980fd13a3047a40512ed1266dfa0f9195c9aec2
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-01-17 21:36:20 +0530

    refactor: move Analytics.application to a AndroidUtils.kt
    This is needed to fix the failing test cases.

--- android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/utils/AndroidUtils.kt
+++ android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/utils/AndroidUtils.kt
@@ -1,8 +1,11 @@
 package com.rudderstack.sdk.kotlin.android.utils
 
+import android.app.Application
 import android.os.Build
 import androidx.annotation.ChecksSdkIntAtLeast
+import com.rudderstack.sdk.kotlin.android.Configuration
 import com.rudderstack.sdk.kotlin.android.utils.AppSDKVersion.getVersionSDKInt
+import com.rudderstack.sdk.kotlin.core.Analytics
 
 /**
  * Executes the provided lambda function based on the current SDK version.
@@ -37,3 +40,14 @@ internal object AppSDKVersion {
         return Build.VERSION.SDK_INT
     }
 }
+
+/**
+ * Provides access to the [Application] instance associated with the Android Analytics object.
+ *
+ * This property retrieves the application instance from the [Configuration] of the Android Analytics object,
+ * ensuring that the correct application context is available for performing various SDK operations.
+ *
+ * @return The [Application] instance tied to the current [Analytics] configuration.
+ */
+internal val Analytics.application: Application
+    get() = (this.configuration as Configuration).application
