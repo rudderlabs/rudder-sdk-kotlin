1a8c20d0c25aa01cedf2a69f4551ca3d56c6caf8
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-01-17 21:32:58 +0530

    Revert "refactor: move AndroidAnalytics.runOnMainThread in a different file"
    This reverts commit f37a2ce3e3a827d4ddb5f9af13a7f46fe9bcb05c.

--- android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/utils/AnalyticsUtils.kt
+++ android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/utils/AnalyticsUtils.kt
@@ -3,7 +3,12 @@ package com.rudderstack.sdk.kotlin.android.utils
 import android.app.Application
 import com.rudderstack.sdk.kotlin.android.Configuration
 import com.rudderstack.sdk.kotlin.core.Analytics
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import com.rudderstack.sdk.kotlin.android.Analytics as AndroidAnalytics
+
+private val MAIN_DISPATCHER = Dispatchers.Main.immediate
 
 /**
  * Runs a suspend block on a coroutine launched in `analyticsScope` with `analyticsDispatcher`
@@ -11,6 +16,16 @@ import kotlinx.coroutines.launch
  * @param block The suspend block which needs to be executed.
  */
 internal fun Analytics.runOnAnalyticsThread(block: suspend () -> Unit) = analyticsScope.launch(analyticsDispatcher) {
+    block()
+}
+
+/**
+ * Runs a block on the main thread.
+ *
+ * @param block The block which needs to be executed.
+ */
+@DelicateCoroutinesApi
+internal fun AndroidAnalytics.runOnMainThread(block: suspend () -> Unit) = analyticsScope.launch(MAIN_DISPATCHER) {
     block()
 }
 
