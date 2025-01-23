22965ebe76199d15654c336b0e3706d4bad98af5
Vishal Gupta <vishal@rudderstack.com>
2025-01-23 14:38:32 +0530

    test: update EventFilteringPluginTest with more test scenarios

--- android/src/test/kotlin/com/rudderstack/sdk/kotlin/android/plugins/devicemode/eventprocessing/EventFilteringPluginTest.kt
+++ android/src/test/kotlin/com/rudderstack/sdk/kotlin/android/plugins/devicemode/eventprocessing/EventFilteringPluginTest.kt
@@ -25,12 +25,18 @@ private const val pathToSourceConfigWithWhiteListEvents =
     "eventfilteringsourceconfig/source_config_with_white_list_event.json"
 private const val pathToSourceConfigWithBlackListEvents =
     "eventfilteringsourceconfig/source_config_with_black_list_event.json"
+private const val pathToSourceConfigWithEmptyWhiteListEvents =
+    "eventfilteringsourceconfig/source_config_with_empty_white_list_events.json"
+private const val pathToSourceConfigWithEmptyBlackListEvents =
+    "eventfilteringsourceconfig/source_config_with_empty_black_list_events.json"
+private const val pathToSourceConfigWithEventFilteringDisabled =
+    "eventfilteringsourceconfig/source_config_with_event_filtering_disabled.json"
 
 class EventFilteringPluginTest {
 
     private val testDispatcher = StandardTestDispatcher()
     private val testScope = TestScope(testDispatcher)
-    private lateinit var plugin: EventFilteringPlugin
+    private lateinit var eventFilteringPlugin: EventFilteringPlugin
     private val mockAnalytics = mockAnalytics(testScope, testDispatcher)
 
     private val sourceConfigWithWhiteListEvents = LenientJson.decodeFromString<SourceConfig>(
@@ -39,13 +45,22 @@ class EventFilteringPluginTest {
     private val sourceConfigWithBlackListEvents = LenientJson.decodeFromString<SourceConfig>(
         readFileAsString(pathToSourceConfigWithBlackListEvents)
     )
+    private val sourceConfigWithEmptyWhiteListEvents = LenientJson.decodeFromString<SourceConfig>(
+        readFileAsString(pathToSourceConfigWithEmptyWhiteListEvents)
+    )
+    private val sourceConfigWithEmptyBlackListEvents = LenientJson.decodeFromString<SourceConfig>(
+        readFileAsString(pathToSourceConfigWithEmptyBlackListEvents)
+    )
+    private val sourceConfigWithEventFilteringDisabled = LenientJson.decodeFromString<SourceConfig>(
+        readFileAsString(pathToSourceConfigWithEventFilteringDisabled)
+    )
 
     @Before
     fun setup() {
-        plugin = EventFilteringPlugin("MockDestination")
+        eventFilteringPlugin = EventFilteringPlugin("MockDestination")
 
         every { mockAnalytics.sourceConfigState } returns FlowState(initialState = SourceConfig.initialState())
-        plugin.setup(mockAnalytics)
+        eventFilteringPlugin.setup(mockAnalytics)
     }
 
     @Test
@@ -55,7 +70,7 @@ class EventFilteringPluginTest {
             testDispatcher.scheduler.advanceUntilIdle()
 
             val allowedEvent = TrackEvent("Track Event 1", emptyJsonObject)
-            val returnedEvent = plugin.intercept(allowedEvent)
+            val returnedEvent = eventFilteringPlugin.intercept(allowedEvent)
 
             assertEquals(allowedEvent, returnedEvent)
         }
@@ -67,7 +82,7 @@ class EventFilteringPluginTest {
             testDispatcher.scheduler.advanceUntilIdle()
 
             val disallowedEvent = TrackEvent("Track Event 3", emptyJsonObject)
-            val returnedEvent = plugin.intercept(disallowedEvent)
+            val returnedEvent = eventFilteringPlugin.intercept(disallowedEvent)
 
             assertNull(returnedEvent)
         }
@@ -79,7 +94,7 @@ class EventFilteringPluginTest {
             testDispatcher.scheduler.advanceUntilIdle()
 
             val disallowedEvent = TrackEvent("Track Event 2", emptyJsonObject)
-            val returnedEvent = plugin.intercept(disallowedEvent)
+            val returnedEvent = eventFilteringPlugin.intercept(disallowedEvent)
 
             assertNull(returnedEvent)
         }
@@ -91,33 +106,78 @@ class EventFilteringPluginTest {
             testDispatcher.scheduler.advanceUntilIdle()
 
             val allowedEvent = TrackEvent("Track Event 3", emptyJsonObject)
-            val returnedEvent = plugin.intercept(allowedEvent)
+            val returnedEvent = eventFilteringPlugin.intercept(allowedEvent)
 
             assertEquals(allowedEvent, returnedEvent)
         }
 
     @Test
     fun `given a sourceConfig with whiteListedEvents, when plugin's intercept is called with an event with leading and trailing spaces but present in list, then it returns the event`() =
-        runTest {
+        runTest(testDispatcher) {
             mockAnalytics.sourceConfigState.dispatch(SourceConfig.UpdateAction(sourceConfigWithWhiteListEvents))
             testDispatcher.scheduler.advanceUntilIdle()
 
             val allowedEvent = TrackEvent(" Track Event 1  ", emptyJsonObject)
-            val returnedEvent = plugin.intercept(allowedEvent)
+            val returnedEvent = eventFilteringPlugin.intercept(allowedEvent)
 
             assertEquals(allowedEvent, returnedEvent)
         }
 
     @Test
     fun `given a sourceConfig with blackListedEvents, when plugin's intercept is called with an event with leading and trailing spaces but present in list, then it returns null`() =
-        runTest {
+        runTest(testDispatcher) {
             mockAnalytics.sourceConfigState.dispatch(SourceConfig.UpdateAction(sourceConfigWithBlackListEvents))
             testDispatcher.scheduler.advanceUntilIdle()
 
             val disallowedEvent = TrackEvent(" Track Event 2  ", emptyJsonObject)
-            val returnedEvent = plugin.intercept(disallowedEvent)
+            val returnedEvent = eventFilteringPlugin.intercept(disallowedEvent)
 
             assertNull(returnedEvent)
+        }
+
+    @Test
+    fun `given a sourceConfig with empty whiteListEvents, when plugin's intercept is called with any event, then it returns null`() =
+        runTest(testDispatcher) {
+            mockAnalytics.sourceConfigState.dispatch(SourceConfig.UpdateAction(sourceConfigWithEmptyWhiteListEvents))
+            testDispatcher.scheduler.advanceUntilIdle()
+
+            val event1 = TrackEvent("Track Event 1", emptyJsonObject)
+            val event2 = TrackEvent("Track Event 2", emptyJsonObject)
+            val event3 = TrackEvent("Track Event 3", emptyJsonObject)
+
+            assertNull(eventFilteringPlugin.intercept(event1))
+            assertNull(eventFilteringPlugin.intercept(event2))
+            assertNull(eventFilteringPlugin.intercept(event3))
+        }
+
+    @Test
+    fun `given a sourceConfig with empty blackListEvents, when plugin's intercept is called with any event, then it returns that event`() =
+        runTest(testDispatcher) {
+            mockAnalytics.sourceConfigState.dispatch(SourceConfig.UpdateAction(sourceConfigWithEmptyBlackListEvents))
+            testDispatcher.scheduler.advanceUntilIdle()
+
+            val event1 = TrackEvent("Track Event 1", emptyJsonObject)
+            val event2 = TrackEvent("Track Event 2", emptyJsonObject)
+            val event3 = TrackEvent("Track Event 3", emptyJsonObject)
+
+            assertEquals(event1, eventFilteringPlugin.intercept(event1))
+            assertEquals(event2, eventFilteringPlugin.intercept(event2))
+            assertEquals(event3, eventFilteringPlugin.intercept(event3))
+        }
+
+    @Test
+    fun `given a sourceConfig with event filtering disabled, when plugin's intercept is called with any event, then it returns the same event`() =
+        runTest {
+            mockAnalytics.sourceConfigState.dispatch(SourceConfig.UpdateAction(sourceConfigWithEventFilteringDisabled))
+            testDispatcher.scheduler.advanceUntilIdle()
+
+            val event1 = TrackEvent("Track Event 1", emptyJsonObject)
+            val event2 = TrackEvent("Track Event 2", emptyJsonObject)
+            val event3 = TrackEvent("Track Event 3", emptyJsonObject)
+
+            assertEquals(event1, eventFilteringPlugin.intercept(event1))
+            assertEquals(event2, eventFilteringPlugin.intercept(event2))
+            assertEquals(event3, eventFilteringPlugin.intercept(event3))
         }
 
     @Test
@@ -132,10 +192,10 @@ class EventFilteringPluginTest {
             identifyEvent.userId = "123"
             val aliasEvent = AliasEvent(previousId = "123")
 
-            assertNotNull(plugin.intercept(screenEvent))
-            assertNotNull(plugin.intercept(groupEvent))
-            assertNotNull(plugin.intercept(identifyEvent))
-            assertNotNull(plugin.intercept(aliasEvent))
+            assertNotNull(eventFilteringPlugin.intercept(screenEvent))
+            assertNotNull(eventFilteringPlugin.intercept(groupEvent))
+            assertNotNull(eventFilteringPlugin.intercept(identifyEvent))
+            assertNotNull(eventFilteringPlugin.intercept(aliasEvent))
         }
 
     @Test
@@ -147,13 +207,13 @@ class EventFilteringPluginTest {
             val event1 = TrackEvent("Track Event 1", emptyJsonObject)
             val event2 = TrackEvent("Track Event 3", emptyJsonObject)
 
-            assertEquals(event1, plugin.intercept(event1))
-            assertNull(plugin.intercept(event2))
+            assertEquals(event1, eventFilteringPlugin.intercept(event1))
+            assertNull(eventFilteringPlugin.intercept(event2))
 
             mockAnalytics.sourceConfigState.dispatch(SourceConfig.UpdateAction(sourceConfigWithBlackListEvents))
             testDispatcher.scheduler.advanceUntilIdle()
 
-            assertNull(plugin.intercept(event1))
-            assertEquals(event2, plugin.intercept(event2))
+            assertNull(eventFilteringPlugin.intercept(event1))
+            assertEquals(event2, eventFilteringPlugin.intercept(event2))
         }
 }
