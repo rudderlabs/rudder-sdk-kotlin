6bb728eb383351464b90475ac98767841fee3333
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-02-06 05:40:31 +0530

    chore(adjust): remove unused dependencies

--- integrations/adjust/build.gradle.kts
+++ integrations/adjust/build.gradle.kts
@@ -56,9 +56,5 @@ dependencies {
     detektPlugins(libs.detekt.formatting)
 
     implementation(libs.android.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
