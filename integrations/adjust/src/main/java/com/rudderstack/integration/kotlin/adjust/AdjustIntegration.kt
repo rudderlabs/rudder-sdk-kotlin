1eaf317857609e5cbe3449ed46969b3699e970eb
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-02-06 19:42:06 +0530

    feat(adjust): implement adjust sdk init and track event

--- integrations/adjust/src/main/java/com/rudderstack/integration/kotlin/adjust/AdjustIntegration.kt
+++ integrations/adjust/src/main/java/com/rudderstack/integration/kotlin/adjust/AdjustIntegration.kt
@@ -1,22 +1,183 @@
 package com.rudderstack.integration.kotlin.adjust
 
-import com.rudderstack.sdk.kotlin.core.Analytics
+import android.app.Activity
+import android.app.Application
+import android.app.Application.ActivityLifecycleCallbacks
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.VisibleForTesting
+import com.adjust.sdk.Adjust
+import com.adjust.sdk.AdjustConfig
+import com.adjust.sdk.AdjustEvent
+import com.adjust.sdk.AdjustInstance
+import com.adjust.sdk.LogLevel
+import com.rudderstack.sdk.kotlin.android.plugins.devicemode.IntegrationPlugin
+import com.rudderstack.sdk.kotlin.android.utils.application
+import com.rudderstack.sdk.kotlin.core.internals.logger.Logger
+import com.rudderstack.sdk.kotlin.core.internals.logger.LoggerAnalytics
 import com.rudderstack.sdk.kotlin.core.internals.models.Event
 import com.rudderstack.sdk.kotlin.core.internals.models.TrackEvent
-import com.rudderstack.sdk.kotlin.core.internals.plugins.Plugin
+import kotlinx.serialization.json.JsonObject
+import com.rudderstack.integration.kotlin.adjust.AdjustConfig as AdjustDestinationConfig
 
 /**
  * AdjustIntegration is a plugin that intercepts the track events and logs them.
  */
-class AdjustIntegration : Plugin {
-    override val pluginType = Plugin.PluginType.OnProcess
+class AdjustIntegration : IntegrationPlugin() {
 
-    override lateinit var analytics: Analytics
+    override val key: String
+        get() = "Adjust"
 
-    override suspend fun intercept(event: Event): Event {
-        if (event is TrackEvent) {
-            println("AdjustIntegration: Track event is made ${event.event}")
+    private val adjustInstance: AdjustInstance? = null
+
+    // TODO("We need a way to update this value dynamically.")
+    private var adjustConfig: AdjustDestinationConfig? = null
+
+    override fun create(destinationConfig: JsonObject,) {
+        if (adjustInstance == null) {
+            destinationConfig.parseConfig<AdjustDestinationConfig>().let { config ->
+                this.adjustConfig = config
+                initialiseAdjust(
+                    application = analytics.application,
+                    appToken = config.appToken,
+                    logLevel = Logger.LogLevel.VERBOSE,
+                )
+            }
         }
-        return event
     }
+
+    override fun getDestinationInstance(): Any? {
+        return adjustInstance
+    }
+
+    override fun track(payload: TrackEvent): Event {
+        // check pre-defined event map and find out the token for event
+        adjustConfig?.customMappings?.find { it.from == payload.event }?.takeUnless { it.to.isBlank() }
+            ?.let { customMapping ->
+                val eventToken = customMapping.to
+                setSessionParams(payload)
+                AdjustEvent(eventToken).let { adjustEvent ->
+                    adjustEvent.addCallbackParameter(payload.properties)
+                    adjustEvent.setRevenue(payload.properties)
+                    adjustEvent.addCallbackParameter(payload.context.toJsonObject(Constants.TRAITS))
+                    Adjust.trackEvent(adjustEvent)
+                }
+            } ?: run {
+            LoggerAnalytics.debug("AdjustIntegration: Event not found in custom mappings.")
+        }
+
+        return payload
+    }
+
+    private fun setSessionParams(payload: TrackEvent) {
+        with(payload) {
+            Adjust.addGlobalCallbackParameter("anonymousId", anonymousId)
+            userId.takeUnless { it.isBlank() }?.let { userId ->
+                Adjust.addGlobalCallbackParameter("userId", userId)
+            }
+        }
+    }
+    private fun AdjustEvent.addCallbackParameter(jsonObject: JsonObject) {
+        jsonObject.forEach { (key, value) ->
+            addCallbackParameter(key, value.toString())
+        }
+    }
+    private fun AdjustEvent.setRevenue(jsonObject: JsonObject) {
+        if (jsonObject[Constants.REVENUE] != null && jsonObject[jsonObject.getString(Constants.CURRENCY)] != null) {
+            setRevenue(jsonObject.getDouble(Constants.REVENUE), jsonObject.getString(Constants.CURRENCY))
+        }
+    }
+}
+
+@VisibleForTesting
+internal fun initialiseAdjust(application: Application, appToken: String, logLevel: Logger.LogLevel,): AdjustInstance {
+    val environment = getEnvironment(logLevel)
+    val adjustConfig = AdjustConfig(application, appToken, environment)
+        .also {
+            it.setLogLevel(logLevel)
+            it.setAllListeners()
+            it.enableSendingInBackground()
+        }
+    Adjust.initSdk(adjustConfig)
+    application.registerActivityLifecycleCallbacks()
+    return Adjust.getDefaultInstance()
+}
+
+private fun getEnvironment(logLevel: Logger.LogLevel): String {
+    return if (logLevel >= Logger.LogLevel.DEBUG) {
+        AdjustConfig.ENVIRONMENT_SANDBOX
+    } else {
+        AdjustConfig.ENVIRONMENT_PRODUCTION
+    }
+}
+
+private fun AdjustConfig.setLogLevel(logLevel: Logger.LogLevel) {
+    when (logLevel) {
+        Logger.LogLevel.VERBOSE -> setLogLevel(LogLevel.VERBOSE)
+        Logger.LogLevel.DEBUG -> setLogLevel(LogLevel.DEBUG)
+        Logger.LogLevel.INFO -> setLogLevel(LogLevel.INFO)
+        Logger.LogLevel.WARN -> setLogLevel(LogLevel.WARN)
+        Logger.LogLevel.ERROR -> setLogLevel(LogLevel.ERROR)
+        else -> setLogLevel(LogLevel.SUPPRESS)
+    }
+}
+
+private fun AdjustConfig.setAllListeners() {
+    setOnAttributionChangedListener({ attribution ->
+        Log.d("AdjustFactory", "Attribution callback called!")
+        Log.d("AdjustFactory", "Attribution: $attribution")
+    })
+    setOnEventTrackingSucceededListener({ adjustEventSuccess ->
+        Log.d("AdjustFactory", "Event success callback called!")
+        Log.d("AdjustFactory", "Event success data: $adjustEventSuccess")
+    })
+    setOnEventTrackingFailedListener { adjustEventFailure ->
+        Log.d("AdjustFactory", "Event failure callback called!")
+        Log.d("AdjustFactory", "Event failure data: $adjustEventFailure")
+    }
+    setOnSessionTrackingSucceededListener { adjustSessionSuccess ->
+        Log.d("AdjustFactory", "Session success callback called!")
+        Log.d("AdjustFactory", "Session success data: $adjustSessionSuccess")
+    }
+    setOnSessionTrackingFailedListener { adjustSessionFailure ->
+        Log.d("AdjustFactory", "Session failure callback called!")
+        Log.d("AdjustFactory", "Session failure data: $adjustSessionFailure")
+    }
+    setOnDeferredDeeplinkResponseListener { deeplink ->
+        Log.d("AdjustFactory", "Deferred deep link callback called!")
+        Log.d("AdjustFactory", "Deep link URL: $deeplink")
+        true
+    }
+}
+
+private fun Application.registerActivityLifecycleCallbacks() {
+    registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+        override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
+            // Not supported
+        }
+
+        override fun onActivityStarted(activity: Activity) {
+            // Not supported
+        }
+
+        override fun onActivityResumed(activity: Activity) {
+            Adjust.onResume()
+        }
+
+        override fun onActivityPaused(activity: Activity) {
+            Adjust.onPause()
+        }
+
+        override fun onActivityStopped(activity: Activity) {
+            // Not supported
+        }
+
+        override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) {
+            // Not supported
+        }
+
+        override fun onActivityDestroyed(activity: Activity) {
+            // Not supported
+        }
+    })
 }
