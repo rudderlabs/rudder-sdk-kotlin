75ebceaf5712a2a5a61fdccdf13bac25bf09fe6a
Vishal Gupta <vishal@rudderstack.com>
2025-02-12 14:34:56 +0530

    refactor: improve code for FirebaseIntegration

--- integrations/firebase/src/main/kotlin/com/rudderstack/integration/kotlin/firebase/FirebaseIntegration.kt
+++ integrations/firebase/src/main/kotlin/com/rudderstack/integration/kotlin/firebase/FirebaseIntegration.kt
@@ -18,7 +18,6 @@ import com.rudderstack.sdk.kotlin.core.internals.models.ScreenEvent
 import com.rudderstack.sdk.kotlin.core.internals.models.TrackEvent
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
-import org.json.JSONException
 
 internal const val FIREBASE_KEY = "Firebase"
 
@@ -75,20 +74,12 @@ class FirebaseIntegration : IntegrationPlugin() {
     }
 
     override fun track(payload: TrackEvent): Event? {
-        val eventName = payload.event
+        val eventName = payload.event.takeIf { it.isNotEmpty() } ?: return payload
 
-        if (eventName.isEmpty()) {
-            return payload
-        }
-
-        if (eventName == "Application Opened") {
-            handleApplicationOpenedEvent(payload.properties)
-        } else if (ECOMMERCE_EVENTS_MAPPING.contains(eventName)) {
-            // handle e-commerce events
-            handleECommerceEvent(eventName, payload.properties)
-        } else {
-            // handle custom events
-            handleCustomEvent(eventName, payload.properties)
+        when (eventName) {
+            "Application Opened" -> handleApplicationOpenedEvent(payload.properties)
+            in ECOMMERCE_EVENTS_MAPPING -> handleECommerceEvent(eventName, payload.properties)
+            else -> handleCustomEvent(eventName, payload.properties)
         }
 
         return payload
@@ -105,31 +96,33 @@ class FirebaseIntegration : IntegrationPlugin() {
     }
 
     private fun handleECommerceEvent(eventName: String, properties: JsonObject?) {
+        val firebaseEvent = ECOMMERCE_EVENTS_MAPPING[eventName] ?: return
+        if (firebaseEvent.isEmpty() || properties.isNullOrEmpty()) return
+
         val params = Bundle()
-        val firebaseEvent: String = ECOMMERCE_EVENTS_MAPPING[eventName] ?: return
-        if (firebaseEvent.isNotEmpty() && !properties.isNullOrEmpty()) {
-            if (firebaseEvent == FirebaseAnalytics.Event.SHARE) {
-                if (!properties.isKeyEmpty("cart_id")) {
-                    params.putString(FirebaseAnalytics.Param.ITEM_ID, properties.getString("cart_id"))
-                } else if (!properties.isKeyEmpty("product_id")) {
-                    params.putString(FirebaseAnalytics.Param.ITEM_ID, properties.getString("product_id"))
-                }
+
+        when (firebaseEvent) {
+            FirebaseAnalytics.Event.SHARE -> {
+                params.putIfNotEmpty(FirebaseAnalytics.Param.ITEM_ID, properties, "cart_id", "product_id")
             }
-            if (firebaseEvent == FirebaseAnalytics.Event.VIEW_PROMOTION || firebaseEvent == FirebaseAnalytics.Event.SELECT_PROMOTION) {
-                if (!properties.isKeyEmpty("name")) {
-                    params.putString(FirebaseAnalytics.Param.PROMOTION_NAME, properties.getString("name"))
-                }
+            FirebaseAnalytics.Event.VIEW_PROMOTION, FirebaseAnalytics.Event.SELECT_PROMOTION -> {
+                params.putIfNotEmpty(FirebaseAnalytics.Param.PROMOTION_NAME, properties, "name")
             }
-            if (firebaseEvent == FirebaseAnalytics.Event.SELECT_CONTENT) {
-                if (!properties.isKeyEmpty("product_id")) {
-                    params.putString(FirebaseAnalytics.Param.ITEM_ID, properties.getString("product_id"))
-                }
+            FirebaseAnalytics.Event.SELECT_CONTENT -> {
+                params.putIfNotEmpty(FirebaseAnalytics.Param.ITEM_ID, properties, "product_id")
                 params.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "product")
             }
-            addConstantParamsForECommerceEvent(params, eventName)
-            handleECommerceEventProperties(params, properties, firebaseEvent)
         }
+
+        addConstantParamsForECommerceEvent(params, eventName)
+        handleECommerceEventProperties(params, properties, firebaseEvent)
         makeFirebaseEvent(firebaseEvent, params, properties)
+    }
+
+    private fun Bundle.putIfNotEmpty(key: String, properties: JsonObject, vararg possibleKeys: String) {
+        possibleKeys.firstOrNull { !properties.isKeyEmpty(it) }?.let {
+            this.putString(key, properties.getString(it))
+        }
     }
 
     private fun handleCustomEvent(eventName: String, properties: JsonObject?) {
@@ -147,67 +140,45 @@ class FirebaseIntegration : IntegrationPlugin() {
     }
 
     private fun handleECommerceEventProperties(params: Bundle, properties: JsonObject, firebaseEvent: String) {
-        if (!properties.isKeyEmpty("revenue") && properties.isDouble("revenue")) {
-            params.putDouble(FirebaseAnalytics.Param.VALUE, properties.getDouble("revenue") ?: 0.0)
-        } else if (!properties.isKeyEmpty("value") && properties.isDouble("value")) {
-            params.putDouble(FirebaseAnalytics.Param.VALUE, properties.getDouble("value") ?: 0.0)
-        } else if (!properties.isKeyEmpty("total") && properties.isDouble("total")) {
-            params.putDouble(FirebaseAnalytics.Param.VALUE, properties.getDouble("total") ?: 0.0)
-        }
+        putDoubleIfValid(params, FirebaseAnalytics.Param.VALUE, properties, listOf("revenue", "value", "total"))
+
         if (EVENT_WITH_PRODUCTS_ARRAY.contains(firebaseEvent) && !properties.isKeyEmpty(ECommerceParamNames.PRODUCTS)) {
             handleProducts(params, properties, true)
         }
         if (EVENT_WITH_PRODUCTS_AT_ROOT.contains(firebaseEvent)) {
             handleProducts(params, properties, false)
         }
-        for (propertyKey in properties.keys) {
-            if (ECOMMERCE_PROPERTY_MAPPING.containsKey(propertyKey) && !properties.isKeyEmpty(propertyKey)) {
-                params.putString(ECOMMERCE_PROPERTY_MAPPING[propertyKey], properties.getString(propertyKey))
+
+        properties.keys.forEach { key ->
+            ECOMMERCE_PROPERTY_MAPPING[key]?.takeIf { !properties.isKeyEmpty(key) }?.let {
+                params.putString(it, properties.getString(key))
             }
         }
-        // Set default Currency to USD, if it is not present in the payload
-        if (properties.containsKey("currency") && !properties.isKeyEmpty("currency")) {
-            params.putString(FirebaseAnalytics.Param.CURRENCY, properties.getString("currency"))
-        } else {
-            params.putString(FirebaseAnalytics.Param.CURRENCY, "USD")
-        }
-        if (!properties.isKeyEmpty("shipping") && properties.isDouble("shipping")) {
-            params.putDouble(FirebaseAnalytics.Param.SHIPPING, properties.getDouble("shipping") ?: 0.0)
-        }
-        if (!properties.isKeyEmpty("tax") && properties.isDouble("tax")) {
-            params.putDouble(FirebaseAnalytics.Param.TAX, properties.getDouble("tax") ?: 0.0)
-        }
-        // order_id is being mapped to FirebaseAnalytics.Param.TRANSACTION_ID.∂
-        if (!properties.isKeyEmpty("order_id")) {
-            params.putString(FirebaseAnalytics.Param.TRANSACTION_ID, properties.getString("order_id"))
-            // As this change is made at version 2.0.2. So to have the backward compatibility, we're inserting order_id properties as well.
-            params.putString("order_id", properties.getString("order_id"))
+
+        params.putString(
+            FirebaseAnalytics.Param.CURRENCY,
+            properties.getString("currency")?.takeIf { !properties.isKeyEmpty("currency") } ?: "USD"
+        )
+
+        putDoubleIfValid(params, FirebaseAnalytics.Param.SHIPPING, properties, listOf("shipping"))
+        putDoubleIfValid(params, FirebaseAnalytics.Param.TAX, properties, listOf("tax"))
+
+        properties.getString("order_id")?.takeIf { !properties.isKeyEmpty("order_id") }?.let {
+            params.putString(FirebaseAnalytics.Param.TRANSACTION_ID, it)
+            params.putString("order_id", it) // For backward compatibility
         }
     }
 
     private fun handleProducts(params: Bundle, properties: JsonObject, isProductsArray: Boolean) {
-        // If Products array is present
         if (isProductsArray) {
-            val products = properties[ECommerceParamNames.PRODUCTS] as? JsonArray ?: return
-            if (products.isNotEmpty()) {
+            (properties[ECommerceParamNames.PRODUCTS] as? JsonArray)?.takeIf { it.isNotEmpty() }?.let { products ->
                 val mappedProducts = ArrayList<Bundle>()
-                for (i in 0 until products.size) {
-                    try {
-                        val product = products[i] as JsonObject
-                        val productBundle = Bundle()
-                        for (key in PRODUCT_PROPERTIES_MAPPING.keys) {
-                            if (!product.isKeyEmpty(key)) {
-                                putProductValue(productBundle, PRODUCT_PROPERTIES_MAPPING[key]!!, product[key])
-                            }
-                        }
+                products.forEach { item ->
+                    (item as? JsonObject)?.let { product ->
+                        val productBundle = mapProductProperties(product)
                         if (!productBundle.isEmpty) {
                             mappedProducts.add(productBundle)
                         }
-                    } catch (e: JSONException) {
-                        LoggerAnalytics.debug("Error while getting Products: $products")
-                    } catch (e: ClassCastException) {
-                        // If products contains list of null value
-                        LoggerAnalytics.debug("Error while getting Products: $products")
                     }
                 }
                 if (mappedProducts.isNotEmpty()) {
@@ -215,53 +186,46 @@ class FirebaseIntegration : IntegrationPlugin() {
                 }
             }
         } else {
-            val productBundle = Bundle()
-            for (key in PRODUCT_PROPERTIES_MAPPING.keys) {
-                if (properties.containsKey(key)) {
-                    putProductValue(
-                        productBundle,
-                        PRODUCT_PROPERTIES_MAPPING[key]!!,
-                        properties[key]
-                    )
-                }
-            }
+            val productBundle = mapProductProperties(properties)
             if (!productBundle.isEmpty) {
                 params.putParcelableArray(FirebaseAnalytics.Param.ITEMS, arrayOf(productBundle))
             }
         }
     }
 
-    private fun putProductValue(params: Bundle, firebaseKey: String, value: Any?) {
-        if (value == null) {
-            return
+    private fun mapProductProperties(properties: JsonObject): Bundle {
+        val bundle = Bundle()
+        PRODUCT_PROPERTIES_MAPPING.forEach { (key, firebaseKey) ->
+            properties[key]?.takeIf { !properties.isKeyEmpty(key) }?.let {
+                putProductValue(bundle, firebaseKey, it)
+            }
         }
+        return bundle
+    }
+
+    private fun putDoubleIfValid(params: Bundle, key: String, properties: JsonObject, keysToCheck: List<String>) {
+        keysToCheck.firstOrNull { !properties.isKeyEmpty(it) && properties.isDouble(it) }?.let {
+            params.putDouble(key, properties.getDouble(it) ?: 0.0)
+        }
+    }
+
+    private fun putProductValue(params: Bundle, firebaseKey: String, value: Any?) {
         when (firebaseKey) {
-            FirebaseAnalytics.Param.ITEM_ID, FirebaseAnalytics.Param.ITEM_NAME, FirebaseAnalytics.Param.ITEM_CATEGORY -> {
-                params.putString(firebaseKey, value.toString())
-                return
-            }
+            FirebaseAnalytics.Param.ITEM_ID,
+            FirebaseAnalytics.Param.ITEM_NAME,
+            FirebaseAnalytics.Param.ITEM_CATEGORY -> params.putString(firebaseKey, value.toString())
 
-            FirebaseAnalytics.Param.QUANTITY -> {
-                (value as? Number)?.let {
-                    params.putLong(firebaseKey, it.toLong())
-                }
-                return
-            }
+            FirebaseAnalytics.Param.QUANTITY -> (value as? Number)?.let { params.putLong(firebaseKey, it.toLong()) }
 
-            FirebaseAnalytics.Param.PRICE -> {
-                (value as? Number)?.let {
-                    params.putDouble(firebaseKey, it.toDouble())
-                }
-                return
-            }
+            FirebaseAnalytics.Param.PRICE -> (value as? Number)?.let { params.putDouble(firebaseKey, it.toDouble()) }
 
-            else -> LoggerAnalytics.debug("Product value is not of expected type")
+            else -> LoggerAnalytics.debug("FirebaseIntegration: Product value is not of expected type")
         }
     }
 
     private fun makeFirebaseEvent(firebaseEvent: String, params: Bundle, properties: JsonObject?) {
         attachAllCustomProperties(params, properties)
-        LoggerAnalytics.debug("Logged \"$firebaseEvent\" to Firebase and properties: $properties")
+        LoggerAnalytics.debug("FirebaseIntegration: Logged \"$firebaseEvent\" to Firebase and properties: $properties")
         firebaseAnalytics?.logEvent(firebaseEvent, params)
     }
 }
