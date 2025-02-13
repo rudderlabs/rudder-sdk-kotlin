6832d95ab07327eb033809cc16de1cb8dd5e12f4
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-02-13 19:36:44 +0530

    feat: implement dynamic config update support

--- integrations/adjust/src/main/java/com/rudderstack/integration/kotlin/adjust/AdjustIntegration.kt
+++ integrations/adjust/src/main/java/com/rudderstack/integration/kotlin/adjust/AdjustIntegration.kt
@@ -35,7 +35,6 @@ class AdjustIntegration : IntegrationPlugin() {
 
     private var adjustInstance: AdjustInstance? = null
 
-    // TODO("We need a way to update this value dynamically.")
     private lateinit var eventToTokenMappings: List<EventToTokenMapping>
 
     public override fun create(destinationConfig: JsonObject) {
@@ -54,6 +53,12 @@ class AdjustIntegration : IntegrationPlugin() {
 
     override fun getDestinationInstance(): Any? {
         return adjustInstance
+    }
+
+    override fun update(destinationConfig: JsonObject) {
+        destinationConfig.parseConfig<AdjustDestinationConfig>().let { updatedConfig ->
+            this.eventToTokenMappings = updatedConfig.eventToTokenMappings
+        }
     }
 
     override fun identify(payload: IdentifyEvent): Event {
