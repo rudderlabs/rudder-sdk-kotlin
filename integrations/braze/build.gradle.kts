31bb0105a008287862021b3796957eb19d7e1cd8
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-02-17 12:59:55 +0530

    chore: add Kotlin serialization plugin to build configuration

--- integrations/braze/build.gradle.kts
+++ integrations/braze/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.detekt)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 detekt {
