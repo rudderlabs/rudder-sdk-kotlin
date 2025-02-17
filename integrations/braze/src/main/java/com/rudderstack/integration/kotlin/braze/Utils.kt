805462fe7e76c690c5f3a1a29aea44383d59103d
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-02-17 12:59:02 +0530

    chore: add utility function to parse JsonObject to specified type

---
+++ integrations/braze/src/main/java/com/rudderstack/integration/kotlin/braze/Utils.kt
@@ -0,0 +1,12 @@
+package com.rudderstack.integration.kotlin.braze
+
+import com.rudderstack.sdk.kotlin.core.internals.utils.InternalRudderApi
+import com.rudderstack.sdk.kotlin.core.internals.utils.LenientJson
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
+
+/**
+ * Parses the [JsonObject] to the specified type [T].
+ */
+@OptIn(InternalRudderApi::class)
+internal inline fun <reified T> JsonObject.parseConfig() = LenientJson.decodeFromJsonElement<T>(this)
