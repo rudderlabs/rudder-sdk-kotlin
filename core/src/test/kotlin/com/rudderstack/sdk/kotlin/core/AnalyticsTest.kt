fce9cd9ce4d0b8074733da5cf3dacd0857bf92fb
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-03-10 17:19:55 +0530

    test: enhance AnalyticsTest with additional event handling tests and mock configurations

--- core/src/test/kotlin/com/rudderstack/sdk/kotlin/core/AnalyticsTest.kt
+++ core/src/test/kotlin/com/rudderstack/sdk/kotlin/core/AnalyticsTest.kt
@@ -1,45 +1,111 @@
 package com.rudderstack.sdk.kotlin.core
 
+import com.rudderstack.sdk.kotlin.core.internals.logger.KotlinLogger
+import com.rudderstack.sdk.kotlin.core.internals.logger.Logger.LogLevel
+import com.rudderstack.sdk.kotlin.core.internals.logger.LoggerAnalytics
 import com.rudderstack.sdk.kotlin.core.internals.models.emptyJsonObject
+import com.rudderstack.sdk.kotlin.core.internals.models.provider.provideSampleJsonPayload
+import com.rudderstack.sdk.kotlin.core.internals.storage.LibraryVersion
 import com.rudderstack.sdk.kotlin.core.internals.storage.Storage
-import com.rudderstack.sdk.kotlin.core.internals.storage.provideBasicStorage
-import com.rudderstack.sdk.kotlin.core.internals.utils.MockMemoryStorage
+import com.rudderstack.sdk.kotlin.core.internals.storage.StorageKeys
+import com.rudderstack.sdk.kotlin.core.internals.utils.DateTimeUtils
 import com.rudderstack.sdk.kotlin.core.internals.utils.empty
+import com.rudderstack.sdk.kotlin.core.internals.utils.generateUUID
+import io.mockk.MockKAnnotations
+import io.mockk.MockKVerificationScope
+import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkObject
 import io.mockk.mockkStatic
 import io.mockk.spyk
+import io.mockk.unmockkObject
+import io.mockk.unmockkStatic
 import io.mockk.verify
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.skyscreamer.jsonassert.JSONAssert
 
 private const val USER_ID = "user-id"
 private val TRAITS: JsonObject = buildJsonObject { put("key-1", "value-1") }
+private const val trackPayloadPath = "messageWitContextObject/track_with_all_arguments_from_server.json"
+private const val screenPayloadPath = "messageWitContextObject/screen_with_all_arguments_from_server.json"
+private const val groupPayloadPath = "messageWitContextObject/group_with_all_arguments_from_server.json"
+private const val identifyPayloadPath = "messageWitContextObject/identify_events_with_all_arguments_from_server.json"
+private const val aliasPayloadPath = "messageWitContextObject/alias_events_with_all_arguments_from_server.json"
 
 class AnalyticsTest {
 
-    private val configuration = provideConfiguration()
+    @MockK
+    private lateinit var mockSourceConfigManager: SourceConfigManager
 
-    private lateinit var analytics: Analytics
+    @MockK
+    private lateinit var mockAnalyticsConfiguration: AnalyticsConfiguration
+
+    @MockK
     private lateinit var mockStorage: Storage
+
+    private val mockCurrentTime = "<original-timestamp>"
+    private val testDispatcher = StandardTestDispatcher()
+    private val testScope = TestScope(testDispatcher)
+
+    private val configuration = provideConfiguration()
+    private lateinit var analytics: Analytics
 
     @BeforeEach
     fun setup() {
-        mockStorage = MockMemoryStorage()
+        MockKAnnotations.init(this, relaxed = true)
 
-        mockkStatic(::provideBasicStorage)
-        every { provideBasicStorage(any()) } returns mockStorage
+        // Mock LoggerAnalytics
+        mockkObject(LoggerAnalytics)
 
+        // Mock Analytics Configuration
+        mockkStatic(::provideAnalyticsConfiguration)
+        every { provideAnalyticsConfiguration(any()) } returns mockAnalyticsConfiguration
+        mockAnalyticsConfiguration.apply {
+            every { analyticsScope } returns testScope
+            every { analyticsDispatcher } returns testDispatcher
+            every { storageDispatcher } returns testDispatcher
+            every { networkDispatcher } returns testDispatcher
+
+            // Mock SourceConfig
+            mockkStatic(::provideSourceConfigManager)
+            every { provideSourceConfigManager(any(), any()) } returns mockSourceConfigManager
+            every { sourceConfigManager } returns mockSourceConfigManager
+
+            every { storage } returns mockStorage
+        }
+
+        // Mocking persisted values and assigning default values
+        every { mockStorage.readString(StorageKeys.ANONYMOUS_ID, defaultVal = any()) } returns ANONYMOUS_ID
+        every { mockStorage.getLibraryVersion() } returns provideLibraryVersion()
+
+        // Mock util method
+        mockkStatic(::generateUUID)
+        every { generateUUID() } returns MESSAGE_ID
+        mockkObject(DateTimeUtils)
+        every { DateTimeUtils.now() } returns mockCurrentTime
+
+        // Analytics instance
         analytics = spyk(Analytics(configuration = configuration))
     }
 
     @Test
     fun `when anonymousId is fetched, then it should return UUID as the anonymousId`() {
+        every { mockStorage.readString(StorageKeys.ANONYMOUS_ID, defaultVal = any()) } returns UUID
+        analytics = spyk(Analytics(configuration = configuration))
+
         val anonymousId = analytics.anonymousId
 
         // This pattern ensures the string follows the UUID v4 format.
@@ -110,6 +176,120 @@ class AnalyticsTest {
         assertNull(userId)
         assertNull(traits)
     }
+
+    @Test
+    fun `when SDK is initialised, then KotlinLogger and default log level should be set`() {
+        verify(exactly = 1) {
+            LoggerAnalytics.setup(any<KotlinLogger>(), LogLevel.NONE)
+        }
+    }
+
+    @Test
+    fun `when SDK is initialised, then SourceConfigManager should be initialised and source config observers should be notified`() {
+        assertNotNull(analytics.sourceConfigManager)
+        assertEquals(mockSourceConfigManager, analytics.sourceConfigManager)
+        verify(exactly = 1) {
+            mockSourceConfigManager.fetchCachedSourceConfigAndNotifyObservers()
+            mockSourceConfigManager.refreshSourceConfigAndNotifyObservers()
+        }
+    }
+
+    @Test
+    fun `given SDK is ready to process any new events, when a track event is made, then it should be stored in the storage`() =
+        runTest(testDispatcher) {
+            val expectedJsonString = readFileTrimmed(trackPayloadPath)
+
+            analytics.track(
+                name = "Track event 1",
+                properties = provideSampleJsonPayload(),
+                options = provideRudderOption(),
+            )
+            testDispatcher.scheduler.runCurrent()
+
+            assertEquals(mockStorage, analytics.storage)
+
+            coVerify(exactly = 1) {
+                mockStorage.write(StorageKeys.EVENT, matchJsonString(expectedJsonString))
+            }
+        }
+
+    @Test
+    fun `given SDK is ready to process any new events, when a screen event is made, then it should be stored in the storage`() =
+        runTest(testDispatcher) {
+            val expectedJsonString = readFileTrimmed(screenPayloadPath)
+
+            analytics.screen(
+                screenName = "Test Screen 1",
+                category = "Main",
+                properties = provideSampleJsonPayload(),
+                options = provideRudderOption(),
+            )
+            testDispatcher.scheduler.runCurrent()
+
+            assertEquals(mockStorage, analytics.storage)
+            coVerify(exactly = 1) {
+                mockStorage.write(StorageKeys.EVENT, matchJsonString(expectedJsonString))
+            }
+        }
+
+    @Test
+    fun `given SDK is ready to process any new events, when a group event is made, then it should be stored in the storage`() =
+        runTest(testDispatcher) {
+            val expectedJsonString = readFileTrimmed(groupPayloadPath)
+
+            analytics.group(
+                groupId = "Group Id 1",
+                traits = provideSampleJsonPayload(),
+                options = provideRudderOption(),
+            )
+            testDispatcher.scheduler.runCurrent()
+
+            assertEquals(mockStorage, analytics.storage)
+            coVerify(exactly = 1) {
+                mockStorage.write(StorageKeys.EVENT, matchJsonString(expectedJsonString))
+            }
+        }
+
+    @Test
+    fun `given SDK is ready to process any new events, when an identify event is made, then it should be stored in the storage`() =
+        runTest(testDispatcher) {
+            val expectedJsonString = readFileTrimmed(identifyPayloadPath)
+
+            analytics.identify(
+                userId = "User Id 1",
+                traits = provideSampleJsonPayload(),
+                options = provideRudderOption(),
+            )
+            testDispatcher.scheduler.runCurrent()
+
+            assertEquals(mockStorage, analytics.storage)
+            coVerify(exactly = 1) {
+                mockStorage.write(StorageKeys.EVENT, matchJsonString(expectedJsonString))
+            }
+        }
+
+    @Test
+    fun `given SDK is ready to process any new events, when an alias event is made, then it should be stored in the storage`() =
+        runTest(testDispatcher) {
+            val expectedJsonString = readFileTrimmed(aliasPayloadPath)
+
+            analytics.alias(
+                newId = "Alias Id 1",
+                previousId = "Previous Id 1",
+                options = provideRudderOption(),
+            )
+            testDispatcher.scheduler.runCurrent()
+
+            assertEquals(mockStorage, analytics.storage)
+            coVerify(exactly = 1) {
+                mockStorage.write(StorageKeys.EVENT, matchJsonString(expectedJsonString))
+            }
+        }
+
+    private fun MockKVerificationScope.matchJsonString(expectedJsonString: String) =
+        withArg<String> { actualJsonString ->
+            JSONAssert.assertEquals(expectedJsonString, actualJsonString, true)
+        }
 }
 
 private fun provideConfiguration() =
@@ -117,3 +297,11 @@ private fun provideConfiguration() =
         writeKey = "<writeKey>",
         dataPlaneUrl = "<data_plane_url>",
     )
+
+private fun provideLibraryVersion(): LibraryVersion {
+    return object : LibraryVersion {
+        override fun getPackageName(): String = "com.rudderstack.kotlin.sdk"
+        override fun getVersionName(): String = "1.0.0"
+    }
+}
+
