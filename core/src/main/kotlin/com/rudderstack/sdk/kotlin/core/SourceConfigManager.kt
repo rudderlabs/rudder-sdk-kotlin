fd6360d7f7231f4604af40129454c899e5a5bcaf
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-03-13 14:06:24 +0530

    test: add unit tests for the core Analytics class (#135)
    * test: add event json files
    
    * refactor: change the scope of the SourceConfigManager to allow unit testing it
    
    * refactor: expose sourceConfigManager for improved testability
    
    * refactor: add internal constants and utility functions for improved mock analytics
    
    * test: enhance AnalyticsTest with additional event handling tests and mock configurations
    
    * refactor: remove unused imports from AnalyticsTest for cleaner code
    
    * refactor: replace hardcoded strings with constants in AnalyticsTest for better maintainability
    
    * test: add other unit tests for track event
    
    * test: add screen event handling tests to AnalyticsTest for improved coverage
    
    * test: add group event handling tests to AnalyticsTest for improved coverage
    
    * test: add additional screen event handling tests to AnalyticsTest for improved coverage
    
    * test: add identify and alias event handling tests to AnalyticsTest for improved coverage
    
    * fix: correct typo in RudderStackDataPlanePlugin documentation and remove trailing comma in provideSourceConfigManager function
    
    * test: refactor track event tests in AnalyticsTest to use parameterized tests for improved readability and maintainability
    
    * refactor(test): use parameterized tests
    
    * test: add shutdown test case
    
    * test: add reset functionality test
    
    * test: add shutdown test case to verify event processing and storage
    
    * test: add tests for dynamic plugin addition and removal in Analytics
    
    * test: add test for default connectivity state initialization in Analytics

--- core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/SourceConfigManager.kt
+++ core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/SourceConfigManager.kt
@@ -7,6 +7,7 @@ import com.rudderstack.sdk.kotlin.core.internals.network.HttpClientImpl
 import com.rudderstack.sdk.kotlin.core.internals.platform.PlatformType
 import com.rudderstack.sdk.kotlin.core.internals.statemanagement.State
 import com.rudderstack.sdk.kotlin.core.internals.storage.StorageKeys
+import com.rudderstack.sdk.kotlin.core.internals.utils.InternalRudderApi
 import com.rudderstack.sdk.kotlin.core.internals.utils.LenientJson
 import com.rudderstack.sdk.kotlin.core.internals.utils.Result
 import com.rudderstack.sdk.kotlin.core.internals.utils.empty
@@ -22,7 +23,11 @@ private const val BUILD_VERSION = "bv"
 private const val ANDROID = "android"
 private const val KOTLIN = "kotlin"
 
-internal class SourceConfigManager(
+/**
+ * Manager for handling source config.
+ */
+@InternalRudderApi
+class SourceConfigManager(
     private val analytics: Analytics,
     private val sourceConfigState: State<SourceConfig>,
     private val httpClientFactory: HttpClient = analytics.createGetHttpClientFactory(),
