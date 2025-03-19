bbfc31fefd0882f1363f413843d57499f1e310f3
Vishal Gupta <vguptakota1998@gmail.com>
2025-03-19 14:51:30 +0530

    refactor: make setup method of LoggerAnalytics as internal api

--- core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/logger/LoggerAnalytics.kt
+++ core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/logger/LoggerAnalytics.kt
@@ -1,5 +1,7 @@
 package com.rudderstack.sdk.kotlin.core.internals.logger
 
+import com.rudderstack.sdk.kotlin.core.internals.utils.InternalRudderApi
+
 /**
  * `LoggerAnalytics` is a singleton class that manages the logging instance for the SDK, supporting configurable
  * logger types and log levels. It allows setting up either an Android or Kotlin logger, providing consistent
@@ -41,6 +43,7 @@ object LoggerAnalytics {
      * @param logger The logger instance to use (e.g., `AndroidLogger` or `KotlinLogger`).
      * @param logLevel The log level to activate for the logger, defining the minimum severity of logs to display.
      */
+    @InternalRudderApi
     fun setup(logger: Logger, logLevel: Logger.LogLevel) {
         this.logger = logger
         this.logLevel = logLevel
