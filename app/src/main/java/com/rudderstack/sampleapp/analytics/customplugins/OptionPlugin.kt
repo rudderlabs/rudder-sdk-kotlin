f8cb6d15d822de032cef49c7a3480d4f30eda912
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-03-19 18:13:35 +0530

    chore(custom-plugin): add support for setting of externalId

--- app/src/main/java/com/rudderstack/sampleapp/analytics/customplugins/OptionPlugin.kt
+++ app/src/main/java/com/rudderstack/sampleapp/analytics/customplugins/OptionPlugin.kt
@@ -3,15 +3,43 @@ package com.rudderstack.sampleapp.analytics.customplugins
 import com.rudderstack.sdk.kotlin.core.Analytics
 import com.rudderstack.sdk.kotlin.core.internals.logger.LoggerAnalytics
 import com.rudderstack.sdk.kotlin.core.internals.models.Event
+import com.rudderstack.sdk.kotlin.core.internals.models.ExternalId
 import com.rudderstack.sdk.kotlin.core.internals.models.RudderOption
+import com.rudderstack.sdk.kotlin.core.internals.models.emptyJsonObject
 import com.rudderstack.sdk.kotlin.core.internals.plugins.Plugin
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.decodeFromJsonElement
+import kotlinx.serialization.json.encodeToJsonElement
+import kotlinx.serialization.json.jsonArray
 
 /**
- * A plugin that adds custom context and integrations to each message. Note: External IDs should not be updated here.
+ * A plugin that adds custom context, integrations and externalIds to each message.
  *
- * Add this plugin during SDK initialization to apply custom context and integrations to all messages.
- * It overrides any individual context and integrations set within a message with the provided custom values.
+ * Add this plugin just after the SDK initialization to apply custom context, integrations and externalIds to all messages.
+ *
+ * **NOTE: It overrides any individual context and integrations set within a message with the provided custom values.**
+ *
+ * Add the plugin like this:
+ * ```
+ * analytics.add(
+ *     GlobalRudderOptionPlugin(
+ *         option = RudderOption(
+ *             customContext = buildJsonObject {
+ *                 put("key", "value")
+ *             },
+ *             integrations = buildJsonObject {
+ *                 put("CleverTap", true)
+ *             },
+ *             externalIds = listOf(
+ *                 ExternalId(type = "globalExternalId", id = "someValue"),
+ *             )
+ *         )
+ *     )
+ * )
+ * ```
  *
  * @param option The custom option to be added to each message.
  */
@@ -31,8 +59,8 @@ class OptionPlugin (
     override suspend fun intercept(event: Event): Event {
         addCustomContext(event)
         addIntegrations(event)
-        // NOTE: Don't update the externalIds, as it should be updated only through the Identify event.
-        LoggerAnalytics.verbose("OptionPlugin: Added custom context and integrations to the message.")
+        addExternalIds(event)
+        LoggerAnalytics.verbose("OptionPlugin: Added custom context, integrations and externalIds to the message.")
         return event
     }
 
@@ -42,6 +70,25 @@ class OptionPlugin (
 
     private fun addIntegrations(event: Event) {
         event.integrations = event.integrations mergeWithHigherPriorityTo option.integrations
+    }
+
+    private fun addExternalIds(event: Event) {
+        val externalIds = mergeExternalIds(event)
+        event.context = event.context mergeWithHigherPriorityTo externalIds.toJsonObject()
+    }
+
+    private fun mergeExternalIds(event: Event): List<ExternalId> {
+        val currentExternalIds: List<ExternalId> = event.context["externalId"]?.jsonArray?.map {
+            Json.decodeFromJsonElement(it)
+        } ?: emptyList()
+
+        val currentExternalIdMap = currentExternalIds.associateBy { it.type }.toMutableMap()
+
+        option.externalIds.forEach { newExternalId ->
+            currentExternalIdMap[newExternalId.type] = newExternalId
+        }
+
+        return currentExternalIdMap.values.toList()
     }
 }
 
@@ -54,3 +101,29 @@ infix fun JsonObject.mergeWithHigherPriorityTo(other: JsonObject): JsonObject {
     return JsonObject(this.toMap() + other.toMap())
 }
 
+/**
+ * Converts a list of `ExternalIds` to a JsonObject.
+ *
+ * If the list is empty, the method returns an empty JSON object.
+ * Else the method returns a JSON object with the following structure:
+ *
+ * ```
+ * "externalId": [
+ *     {
+ *         "id": "<some-value>",
+ *         "type": "brazeExternalId"
+ *     },
+ *     {
+ *         "id": "<some-value>",
+ *         "type": "ga4"
+ *     }
+ * ],
+ * ```
+ */
+private fun List<ExternalId>.toJsonObject(): JsonObject {
+    if (this.isEmpty()) return emptyJsonObject
+    val externalIds = Json.encodeToJsonElement(this) as JsonArray
+    return buildJsonObject {
+        put("externalId", externalIds)
+    }
+}
