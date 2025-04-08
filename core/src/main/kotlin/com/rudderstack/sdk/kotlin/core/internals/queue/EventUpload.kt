435f9715832b94b2c22e11a6ea4c0479b44e0168
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-04-08 19:05:23 +0530

    refactor: streamline event upload process by separating upload preparation and event processing

--- core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/queue/EventUpload.kt
+++ core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/queue/EventUpload.kt
@@ -21,10 +21,14 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.annotations.VisibleForTesting
 import java.io.File
 import java.io.FileNotFoundException
+import kotlin.coroutines.coroutineContext
 
 private const val BATCH_ENDPOINT = "/v1/batch"
 private val ANONYMOUS_ID_REGEX = """"anonymousId"\s*:\s*"([^"]+)"""".toRegex()
 
+/**
+ * EventUpload is responsible for uploading events to the RudderStack data plane.
+ */
 internal class EventUpload(
     private val analytics: Analytics,
     private var uploadChannel: Channel<String> = createUnlimitedUploadChannel(),
@@ -63,47 +67,59 @@ internal class EventUpload(
     private fun upload() = analytics.analyticsScope.launch(analytics.networkDispatcher) {
         uploadChannel.consumeEach {
             LoggerAnalytics.debug("performing flush")
-            withContext(analytics.storageDispatcher) {
-                storage.rollover()
+            prepareForUpload()
+            processAndUploadEvent()
+        }
+    }
+
+    private suspend fun prepareForUpload() {
+        withContext(analytics.storageDispatcher) {
+            storage.rollover()
+        }
+    }
+
+    private suspend fun processAndUploadEvent() {
+        val fileUrlList = storage.readString(StorageKeys.EVENT, String.empty()).parseFilePaths()
+        for (filePath in fileUrlList) {
+            val file = File(filePath)
+            if (!doesFileExist(file)) continue
+            // ensureActive is at this position so that this coroutine can be cancelled - but any uploaded event MUST be cleared from storage.
+            coroutineContext.ensureActive()
+            var shouldCleanup = false
+            try {
+                shouldCleanup = uploadEvents(filePath)
+            } catch (e: FileNotFoundException) {
+                LoggerAnalytics.error("Message storage file not found", e)
+            } catch (e: Exception) {
+                LoggerAnalytics.error("Error when uploading event", e)
             }
-            val fileUrlList = storage.readString(StorageKeys.EVENT, String.empty()).parseFilePaths()
-            for (filePath in fileUrlList) {
-                val file = File(filePath)
-                if (!doesFileExist(file)) continue
-                // ensureActive is at this position so that this coroutine can be cancelled - but any uploaded event MUST be cleared from storage.
-                ensureActive()
-                var shouldCleanup = false
-                try {
-                    val batchPayload = jsonSentAtUpdater.updateSentAt(readFileAsString(filePath))
 
-                    checkAndUpdateBatchRequestHeader(batchPayload)
-                    LoggerAnalytics.debug("Batch Payload: $batchPayload")
-                    when (val result: Result<String, Exception> = httpClientFactory.sendData(batchPayload)) {
-                        is Result.Success -> {
-                            LoggerAnalytics.debug("Event uploaded successfully. Server response: ${result.response}")
-                            shouldCleanup = true
-                        }
-
-                        is Result.Failure -> {
-                            LoggerAnalytics.debug("Error when uploading event due to ${result.status} ${result.error}")
-                        }
-                    }
-                } catch (e: FileNotFoundException) {
-                    LoggerAnalytics.error("Message storage file not found", e)
-                } catch (e: Exception) {
-                    LoggerAnalytics.error("Error when uploading event", e)
-                }
-
-                if (shouldCleanup) {
-                    storage.remove(file.path).let {
-                        LoggerAnalytics.debug("Removed file: $filePath")
-                    }
-                }
+            if (shouldCleanup) {
+                cleanup(file, filePath)
             }
         }
     }
 
-    private fun checkAndUpdateBatchRequestHeader(batchPayload: String) {
+    private fun uploadEvents(filePath: String): Boolean {
+        var shouldCleanup = false
+        val batchPayload = jsonSentAtUpdater.updateSentAt(readFileAsString(filePath))
+
+        updateAnonymousIdHeaderIfChanged(batchPayload)
+        LoggerAnalytics.debug("Batch Payload: $batchPayload")
+        when (val result: Result<String, Exception> = httpClientFactory.sendData(batchPayload)) {
+            is Result.Success -> {
+                LoggerAnalytics.debug("Event uploaded successfully. Server response: ${result.response}")
+                shouldCleanup = true
+            }
+
+            is Result.Failure -> {
+                LoggerAnalytics.debug("Error when uploading event due to ${result.status} ${result.error}")
+            }
+        }
+        return shouldCleanup
+    }
+
+    private fun updateAnonymousIdHeaderIfChanged(batchPayload: String) {
         val currentBatchAnonymousId = getAnonymousIdFromBatch(batchPayload)
         if (lastBatchAnonymousId != currentBatchAnonymousId) {
             httpClientFactory.updateAnonymousIdHeaderString(currentBatchAnonymousId.encodeToBase64())
@@ -116,6 +132,12 @@ internal class EventUpload(
         return ANONYMOUS_ID_REGEX.find(batchPayload)?.groupValues?.get(1) ?: run {
             LoggerAnalytics.error("Fetched empty anonymousId from batch payload, falling back to random UUID.")
             generateUUID()
+        }
+    }
+
+    private fun cleanup(file: File, filePath: String) {
+        storage.remove(file.path).let {
+            LoggerAnalytics.debug("Removed file: $filePath")
         }
     }
 
