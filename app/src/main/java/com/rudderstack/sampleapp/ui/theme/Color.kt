aea2cf402d6c3927460dcb91c2713f257af72759
Chryssa Aliferi <c.aliferi@gmail.com>
2025-04-16 18:41:37 +0300

    feat: minor improvements in sample colors

--- app/src/main/java/com/rudderstack/sampleapp/ui/theme/Color.kt
+++ app/src/main/java/com/rudderstack/sampleapp/ui/theme/Color.kt
@@ -2,12 +2,8 @@ package com.rudderstack.sampleapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val White = Color(0xFFFFFFFFF)
-val WhiteTransparency = Color(0x80FFFFFF)
+val White = Color(0xFFFFFFFF)
 val Blue = Color(0xFF105ED5)
 val Black = Color(0xFF000000)
-val Black50 = Color(0x80000000)
-val Grey = Color(0xFFCBCDD8)
-val GreySuperLight = Color(0xFFE0E2E5)
 val GreyLight = Color(0xFF979BB1)
 val Grey50 = Color(0x80979BB1)
