427e952c4fc42e20701965d9899c1141bc0d1c69
Chryssa Aliferi <c.aliferi@gmail.com>
2025-04-28 15:09:47 +0300

    fix: adds LaunchEffect comments

--- app/src/main/java/com/rudderstack/sampleapp/MainActivity.kt
+++ app/src/main/java/com/rudderstack/sampleapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.rememberNavController
 import com.rudderstack.sampleapp.analytics.RudderAnalyticsUtils.analytics
 import com.rudderstack.sampleapp.mainscreen.MainViewModel
@@ -19,9 +20,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-    LaunchedEffect(Unit) {
-        analytics.setNavigationDestinationsTracking(navController, this@MainActivity)
-    }  
+
+            // Analytics tracking is initialized to track navigation events within the app.
+            // Using a LaunchedEffect blocks helps us to avoid getting called multiple times due to recomposition.
+            LaunchedEffect(Unit) {
+                analytics.setNavigationDestinationsTracking(navController, this@MainActivity)
+            }
             RudderAndroidLibsTheme {
                 AppNavHost(viewModel = viewModel, navController = navController)
             }
