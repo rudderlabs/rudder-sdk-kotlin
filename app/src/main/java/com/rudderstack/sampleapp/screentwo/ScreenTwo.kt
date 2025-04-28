2f32ee4af9ca335f8a4d7a6105678ce78806e353
Chryssa Aliferi <c.aliferi@gmail.com>
2025-04-28 16:56:53 +0300

    feat: improve android application sample (#126)
    * refactor: adds material 3 in the sample application
    
    * refactor: adds new launcher icons
    
    * refactor: adds new rudderstack icon
    
    * refactor: removes unused xml files
    
    * refactor: adds new themes.xml
    
    * feat: creates new Type.kt
    
    * feat: creates new Color.kt
    
    * feat: creates new Theme.kt
    
    * feat: updates strings.xml
    
    * feat: updates colors.xml
    
    * feat: updates MainActivity.kt implementation
    
    * feat: minor improvements in sample application theme
    
    * feat: minor improvements in sample colors
    
    * feat: organise libs.versions.toml
    
    * refactor: improves RudderAnalyticsUtils
    
    * feat: adds compose navigation in app build.gradle.kts
    
    * feat: adds AppNavigationGraph and AppNavHost
    
    * feat: creates Routes.kt
    
    * feat: adds ScreenOne.kt
    
    * feat: adds ScreenTwo.kt
    
    * refactor: moves and improves MainActivity.kt
    
    * feat: creates MainScreen
    
    * refactor: improves MainViewModel
    
    * refactor: minor improvements MainViewModelState
    
    * refactor: sets the correct MainActivity in AndroidManifest.xml
    
    * feat: minor UI improvements
    
    * fix: apply fix on setting the correct navController
    
    * Update app/src/main/java/com/rudderstack/sampleapp/MainActivity.kt
    
    Co-authored-by: Vishal Gupta <vguptakota1998@gmail.com>
    
    * fix: adds LaunchEffect comments
    
    * fix: adds a suppress warning on AndroidAdvertisingIdPlugin
    
    ---------
    
    Co-authored-by: Vishal Gupta <vguptakota1998@gmail.com>

---
+++ app/src/main/java/com/rudderstack/sampleapp/screentwo/ScreenTwo.kt
@@ -0,0 +1,53 @@
+package com.rudderstack.sampleapp.screentwo
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.rudderstack.sampleapp.mainscreen.ActionButton
+import com.rudderstack.sampleapp.mainscreen.CreateLogcat
+import com.rudderstack.sampleapp.mainscreen.MainViewModel
+import com.rudderstack.sampleapp.screenone.Title
+
+@Composable
+fun ScreenTwo(
+    viewModel: MainViewModel,
+    onNavigateToScreenOne: () -> Unit
+) {
+    ScreenTwoContent(
+        viewModel = viewModel,
+        onNavigateToScreenOne = onNavigateToScreenOne
+    )
+}
+
+@Composable
+fun ScreenTwoContent(
+    viewModel: MainViewModel,
+    onNavigateToScreenOne: () -> Unit
+) {
+    Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+        Spacer(modifier = Modifier.height(16.dp))
+        Title(text = "Screen Two")
+        Spacer(modifier = Modifier.height(16.dp))
+        NavigationButton(onNavigateToScreenOne = onNavigateToScreenOne)
+        Spacer(modifier = Modifier.height(16.dp))
+        CreateLogcat(viewModel)
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+private fun NavigationButton(onNavigateToScreenOne: () -> Unit) {
+    ActionButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        onClick = onNavigateToScreenOne,
+        eventName = "Navigate to Screen One"
+    )
+}
