5718cd677b7e445c1066dc58887a6ff542246783
Chryssa Aliferi <c.aliferi@gmail.com>
2025-05-20 00:36:02 +0300

    refactor: minor change in AndroidConnectivityObserverPlugin

--- android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/connectivity/AndroidConnectivityObserverPlugin.kt
+++ android/src/main/kotlin/com/rudderstack/sdk/kotlin/android/connectivity/AndroidConnectivityObserverPlugin.kt
@@ -108,7 +108,6 @@ internal fun createNetworkCallback(connectivityState: State<Boolean>) = object :
 @SuppressLint("MissingPermission")
 @Suppress("DEPRECATION")
 internal fun createBroadcastReceiver(connectivityState: State<Boolean>) = object : BroadcastReceiver() {
-    @SuppressLint("MissingPermission")
     override fun onReceive(context: Context, intent: Intent?) {
         (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).activeNetworkInfo?.let {
             when (it.isConnected) {
