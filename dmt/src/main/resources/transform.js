c666f2fd97b5a100b5797bdd5b3c3d9e0e104262
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-08-06 23:55:16 +0530

    refactor: simplify transformEvent function by removing try-catch block

--- dmt/src/main/resources/transform.js
+++ dmt/src/main/resources/transform.js
@@ -1,10 +1,6 @@
 function transformEvent(eventData) {
-    try {
-        if (eventData.event) {
-            eventData.event = eventData.event + "_transform";
-        }
-        return eventData;
-    } catch (e) {
-        return eventData;
+    if (eventData.event) {
+        eventData.event = eventData.event + "_transform";
     }
+    return eventData;
 }
