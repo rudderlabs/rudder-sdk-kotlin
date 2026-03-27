3d7fd2e321f404bc997e4e8923d9b6ced96d5772
Vishal Gupta <vguptakota1998@gmail.com>
2026-03-27 13:22:59 +0530

    chore(core): replace full batch payload logging with payload size in EventUpload
    Scanned-by: gitleaks 8.29.0

--- core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/queue/EventUpload.kt
+++ core/src/main/kotlin/com/rudderstack/sdk/kotlin/core/internals/queue/EventUpload.kt
@@ -136,7 +136,7 @@ internal class EventUpload(
         var result: EventUploadResult
         do {
             val updatedPayload = JsonSentAtUpdater.updateSentAt(batchPayload, analytics.logger)
-            analytics.logger.verbose("EventUpload: Batch payload: $updatedPayload")
+            analytics.logger.verbose("EventUpload: Uploading batch payload of size: ${updatedPayload.length}")
             val currentTimestampInMillis = DateTimeUtils.getSystemCurrentTime()
             val retryHeaders = retryHeadersProvider.getHeaders(batchId, currentTimestampInMillis)
             result = httpClientFactory.sendData(updatedPayload, retryHeaders).toEventUploadResult()
